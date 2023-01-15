268b2d8c352c3b2c9fd1387291262849c361ae6c
10p <garimaarora.ga2@gmail.com>
2023-01-15 10:57:43 +0530

    Added solution - LeetHub

--- Minimum number of Coins - GFG/minimum-number-of-coins.py
+++ Minimum number of Coins - GFG/minimum-number-of-coins.py
@@ -2,18 +2,14 @@
 
 class Solution:
     def minPartition(self, N):
-        
-        # code here 43 23 3
         currency = [1, 2, 5, 10, 20, 50, 100, 200, 500, 2000]
         i = len(currency) - 1
         ans = []
         while N > 0:
             if currency[i]>N:
                 i-=1
-                continue
             else:
                 while currency[i]<=N:
-                    
                     ans.append(currency[i])
                     N-=currency[i]
         return ans
