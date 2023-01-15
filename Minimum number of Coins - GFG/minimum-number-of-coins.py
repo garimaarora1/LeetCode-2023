40755cf177134dc7d4e15770695f74301b45c124
10p <garimaarora.ga2@gmail.com>
2023-01-15 10:57:15 +0530

    Added solution - LeetHub

---
+++ Minimum number of Coins - GFG/minimum-number-of-coins.py
@@ -0,0 +1,35 @@
+#User function Template for python3
+
+class Solution:
+    def minPartition(self, N):
+        
+        # code here 43 23 3
+        currency = [1, 2, 5, 10, 20, 50, 100, 200, 500, 2000]
+        i = len(currency) - 1
+        ans = []
+        while N > 0:
+            if currency[i]>N:
+                i-=1
+                continue
+            else:
+                while currency[i]<=N:
+                    
+                    ans.append(currency[i])
+                    N-=currency[i]
+        return ans
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        
+        ob = Solution()
+        arr = ob.minPartition(N)
+        for i in range(len(arr)):
+            print(arr[i],end=" ")
+        print()
+# } Driver Code Ends
