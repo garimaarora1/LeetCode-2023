78ad2bd4c6c9072c224f0de9d0d4b82afc78e3d6
Garima Arora <garimaarora.ga2@gmail.com>
2023-10-16 21:11:55 +0530

    Time: 51 ms, Memory: 16.4 MB - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,19 +1,18 @@
+from collections import defaultdict
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        i = j = 0
-        nums1.sort()
-        nums2.sort()
+        d1 = defaultdict(int)
+        d2 = defaultdict(int)
         res = []
-        while i < len(nums1) and j < len(nums2):
-            if nums1[i] < nums2[j]:
-                i += 1
-            elif nums2[j] < nums1[i]:
-                j += 1
-            else:
-                res.append(nums1[i])
-                i += 1
-                j += 1
+        for i in nums1:
+            d1[i] += 1
+        for i in nums2:
+            d2[i] += 1
+        for key in d1:
+            if key in d2:
+                val = min(d1[key], d2[key])
+                sub_list = val*[key]
+                res.extend(sub_list)
         return res
-                
             
         
