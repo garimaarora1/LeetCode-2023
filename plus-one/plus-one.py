3b1d62e3d022bf50f6efb01dd76fd72b6896b39b
Garima Arora <garimaarora.ga2@gmail.com>
2023-10-16 21:39:28 +0530

    Time: 49 ms, Memory: 16.1 MB - LeetHub

---
+++ plus-one/plus-one.py
@@ -0,0 +1,19 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        if digits[-1] != 9:
+            digits[-1] += 1
+        else:
+            # 8999
+            i = len(digits) # 4
+            i -=1 # 3
+            while i >=0:
+                if digits[i] == 9:
+                    digits[i] = 0
+                    i -= 1
+                else:
+                    break
+            if i==-1:
+                digits.insert(0,1)
+            else:
+                digits[i] += 1
+        return digits
