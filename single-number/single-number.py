83c0216043e23b5d10d410a192ead08214d28131
Garima Arora <garimaarora.ga2@gmail.com>
2023-10-16 20:50:37 +0530

    Time: 125 ms, Memory: 19 MB - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,8 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        x = nums[0]
-        for i in range(1, len(nums)):
+        x = 0
+        for i in range(len(nums)):
             x ^= nums[i]
         return x
             
-        
