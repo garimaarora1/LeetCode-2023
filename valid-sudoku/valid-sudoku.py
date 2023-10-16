00bc1fdbca08115ea5fee02162ce81d07b5f8f2d
Garima Arora <garimaarora.ga2@gmail.com>
2023-10-16 22:42:10 +0530

    Time: 89 ms, Memory: 16.4 MB - LeetHub

--- valid-sudoku/valid-sudoku.py
+++ valid-sudoku/valid-sudoku.py
@@ -7,7 +7,6 @@ class Solution:
                     n = board[i][j]
                     if (i,n) in s or (n, j) in s or (i//3, j//3, n) in s:
                         return False
-                    
                     s.add((i,n))
                     s.add((n, j))
                     s.add((i//3, j//3, n))
