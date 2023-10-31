2b66a37527f8e3232c930e062522d27c0c6351f3
Garima Arora <garimaarora.ga2@gmail.com>
2023-10-31 21:02:24 +0530

    Time: 73 ms (28.61%), Space: 16.4 MB (21.37%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -6,10 +6,8 @@ class Solution:
         j = 0
         for i in range(len(s)):
             d[s[i]] = d.get(s[i], 0) + 1
-            print(2)
             # new ele got added in dict 
             if d[s[i]] == 1:
-                print(1)
                 curr_sub_string_len += 1
                 max_sub_string_len = max(max_sub_string_len, curr_sub_string_len)
             else:
