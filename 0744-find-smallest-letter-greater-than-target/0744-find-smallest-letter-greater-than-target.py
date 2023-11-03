953d303296fd81ddc17cf5d6f5c80c7126e7e6dc
Garima Arora <garimaarora.ga2@gmail.com>
2023-11-03 18:29:13 +0530

    Time: 124 ms (22.28%), Space: 16.5 MB (95.52%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.py
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.py
@@ -5,14 +5,10 @@ class Solution:
         while lo<=hi:
             mid = lo + (hi-lo)//2
             if letters[mid] > target:
-                print("mini:", mini)
-                print("letters[mid]:", letters[mid])
                 if mini <= target:
-                    print(1)
                     mini = letters[mid]
                 else:
                     mini = min(mini, letters[mid])
-                print("final_mini:", mini)
                 hi = mid - 1
             else:
                 lo = mid + 1
