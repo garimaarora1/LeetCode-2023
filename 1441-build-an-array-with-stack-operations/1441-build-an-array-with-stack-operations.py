ffe03423d4cbb929526dc636fdfee3f633f71b1c
Garima Arora <garimaarora.ga2@gmail.com>
2023-11-04 08:51:19 +0530

    Time: 42 ms (43.66%), Space: 16.3 MB (44.47%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
@@ -1,31 +1,12 @@
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
-        if not target:
-            return 
-
-        # result array, contain values "Push", "Pop"
         res = []
-        st = []
-        stream = 1
         i = 0
-
-        while i < len(target):
-            while not st or (st and st[-1]+1 < target[i]):
-                st.append(stream)
+        for num in target:
+            while i < num - 1:
                 res.append("Push")
-                stream += 1
-
-            while st and st[-1] < target[i] and (st[-1] > target[i-1] or i == 0):
-                st.pop()
                 res.append("Pop")
-            
-            if st == target:
-                return res
-
-            st.append(stream)
-            stream += 1
+                i += 1
             res.append("Push")
-            i += 1
+            i += 1   
         return res
-    
-    
