9576a67fc0931cdd575ae08bf229be80169a0524
Garima Arora <garimaarora.ga2@gmail.com>
2024-01-28 16:35:59 +0530

    Time: 43 ms (24.16%), Space: 16.5 MB (70.34%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,17 @@
+class Solution:
+
+    def recursive_pow(self, x, n) -> float:
+        if n == 0:
+            return 1
+        if n % 2 != 0:    
+            return self.recursive_pow(x*x, n//2) * x
+        else:
+            return self.recursive_pow(x*x, n//2)
+
+    def myPow(self, x: float, n: int) -> float: 
+        if n >= 0:
+            return self.recursive_pow(x, abs(n))
+        else:
+            return self.recursive_pow(1/x, abs(n))
+            
+   
