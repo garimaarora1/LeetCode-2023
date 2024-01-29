633e5de0f568404387e620a68adbbbd4bb04d91b
Garima Arora <garimaarora.ga2@gmail.com>
2024-01-29 18:14:06 +0530

    Time: 44 ms (24.14%), Space: 16.6 MB (51.32%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -1,18 +1,16 @@
 class Solution:
+    MOD = (10 ** 9) + 7
     def recursive_pow(self, x, n) -> float:
         if n == 0:
             return 1
         if n % 2 != 0:    
-            return (self.recursive_pow((x*x) % (10**9 + 7), n//2) * x) % (10**9 + 7)
+            return (self.recursive_pow((x*x) % self.MOD, n//2) * x) % self.MOD
         else:
-            return self.recursive_pow((x*x) % (10**9 + 7), n//2) % (10**9 + 7)
+            return self.recursive_pow((x*x) % self.MOD, n//2) % self.MOD
 
     def countGoodNumbers(self, n: int) -> int:
-        if n%2 == 0:
-            o_pow = e_pow = n//2
-        else:
-            o_pow, e_pow = n//2, n//2+1
-        result = (self.recursive_pow(5, e_pow) * self.recursive_pow(4, o_pow)) % (10**9 + 7)
-        return(result)
-
+        odd_pow = n // 2
+        even_pow = (n // 2) + (n % 2)
+        result = (self.recursive_pow(5, even_pow) * self.recursive_pow(4, odd_pow)) % self.MOD
+        return result
 
