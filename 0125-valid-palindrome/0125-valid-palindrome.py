46eabbbaaea61fc9b7e05c2c6aeb033c109cbacd
Garima Arora <garimaarora.ga2@gmail.com>
2024-03-13 14:25:14 +0530

    Time: 45 ms (57.08%), Space: 17 MB (68.05%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,5 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
         i = 0 
         j = len(s) - 1
         while i < j:
@@ -7,7 +8,7 @@ class Solution:
                 i += 1
             while i<j and not s[j].isalnum():
                 j -= 1
-            if s[i].lower() != s[j].lower():
+            if s[i] != s[j]:
                 return False
             i += 1
             j -= 1
