4d433af6c004c84e145a3aae5f8af4ce9e23e12a
Garima Arora <garimaarora.ga2@gmail.com>
2024-03-13 11:14:40 +0530

    Time: 48 ms (64.34%), Space: 16.8 MB (51.38%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -2,18 +2,16 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         ans = []
         st = []
+        d = {}
         for i in range(len(nums2)-1, -1, -1):
             while st and st[-1] <= nums2[i]:
                 st.pop()
             if not st:
-                ans.append(-1)
+                d[nums2[i]] = (-1)
             elif st and st[-1] > nums2[i]:
-                ans.append(st[-1])
+                d[nums2[i]] = (st[-1])
             st.append(nums2[i])
-        ans = ans[::-1]
         
-        final_ans = []
         for num in nums1:
-            i = nums2.index(num)
-            final_ans.append(ans[i])
-        return final_ans
+            ans.append(d[num])
+        return ans
