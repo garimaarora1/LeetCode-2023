9513010b3e87e011b4b9584d5a0095b9a2330113
Garima Arora <garimaarora.ga2@gmail.com>
2024-03-13 10:42:12 +0530

    Time: 177 ms (12.45%), Space: 18.1 MB (39.13%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 1 
+        majority_element = nums[0]
+        for i in range(1, len(nums)):
+            if count == 0:
+                majority_element = nums[i]
+            count += 1 if nums[i] == majority_element else -1
+        return majority_element
+        
+        
+        
