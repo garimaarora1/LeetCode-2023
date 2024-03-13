9e9a5ec17a0fd96d91c9f8ebf437e03011b2cb9f
Garima Arora <garimaarora.ga2@gmail.com>
2024-03-13 15:48:47 +0530

    Time: 41 ms (20.32%), Space: 16.6 MB (32.46%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,15 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        parentheses_dict = {')': '(', '}': '{', ']': '[' }
         st = []
-        d = { ')': '(', ']': '[', '}': '{'}
-        for i in s:
-            if i in d.keys():
-                if st and st[-1] == d[i]:
+        
+        for char in s:
+            if char in parentheses_dict.values():
+                st.append(char)
+            else:
+                if st and st[-1] == parentheses_dict[char]:
                     st.pop()
                 else:
                     return False
-            else:
-                st.append(i)
         return True if not st else False
-                
-        
