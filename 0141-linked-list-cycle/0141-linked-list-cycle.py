c8c2d39d22b2fb9cfa4a82d2f0e5546aca3c2b82
Garima Arora <garimaarora.ga2@gmail.com>
2024-03-18 19:54:28 +0530

    Time: 43 ms (73.11%), Space: 19.2 MB (34.76%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -3,7 +3,15 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
+        
+"""      
+Explanation from discuss section:
+If the track is 100m long, your speed is 10m/s, your friend's speed is 5m/s. Then after 20s, you've run 200m, your friend has run 100m. But because the track is circular, so you will be at the same place with your friend since the difference between your distances is exactly 100m.
 
+How about we change another track, change the speed of you and your friend? As long as your speeds are not the same, the faster person will always catch up with the slower person again.
+
+https://leetcode.com/problems/linked-list-cycle/discuss/1829768/JavaScript-Easy-to-understand-slow-and-fast-pointers-detailed-explanation
+"""
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         slow = head
@@ -15,3 +23,13 @@ class Solution:
                 return True
         return False
         
+        
+
+        
+        
+"""      
+Explanation from discuss section:
+If the track is 100m long, your speed is 10m/s, your friend's speed is 5m/s. Then after 20s, you've run 200m, your friend has run 100m. But because the track is circular, so you will be at the same place with your friend since the difference between your distances is exactly 100m.
+
+How about we change another track, change the speed of you and your friend? As long as your speeds are not the same, the faster person will always catch up with the slower person again.
+"""
