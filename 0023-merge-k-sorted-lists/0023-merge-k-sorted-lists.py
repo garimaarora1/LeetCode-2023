6c05eb462ba58cf6b5508916878adc89009b8ed8
Garima Arora <garimaarora.ga2@gmail.com>
2024-03-18 23:04:31 +0530

    Time: 73 ms (81.37%), Space: 19.4 MB (51.28%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,6 +3,25 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+"""
+
+Approach:
+
+Take the first node of each of the linked lists
+and add it into a heap. When you add it to the heap
+add (node.val, i) where i is the ith list.
+
+Create a dummy node head.
+
+Pop the first node from the heap and make it the
+next node in the dummy-list. Remember to add the
+first node from the ith linked list into the heap
+since we just removed a node from this list from the heap.
+
+Repeat until the heap is empty.
+
+https://leetcode.com/problems/merge-k-sorted-lists/discuss/1032502/Python-Simple-Heap-Solution
+"""
 class Solution:
         
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
