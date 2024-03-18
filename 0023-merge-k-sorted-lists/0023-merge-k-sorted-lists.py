68fdfc91241dfdd62b1e510dca82ca5b0e2cab8e
Garima Arora <garimaarora.ga2@gmail.com>
2024-03-18 21:17:53 +0530

    Time: 73 ms (81.37%), Space: 19.2 MB (92.57%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -4,6 +4,19 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+        
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if len(lists) == 0:
+            return None
+        while len(lists) > 1:
+            merged_lists = []
+            for i in range(0, len(lists), 2):
+                list1 = lists[i]
+                list2 = lists[i+1] if i+1 < len(lists) else None
+                merged_lists.append(self.merge_two_lists(list1, list2))
+            lists = merged_lists
+        return lists[0]
+
     def merge_two_lists(self, list1, list2):
         dummy_node = ListNode()
         merged_list = dummy_node
@@ -20,16 +33,4 @@ class Solution:
         if list2:
             dummy_node.next = list2
         return merged_list.next
-        
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if len(lists) == 0:
-            return None
-        while len(lists) > 1:
-            merged_lists = []
-            for i in range(0, len(lists), 2):
-                list1 = lists[i]
-                list2 = lists[i+1] if i+1 < len(lists) else None
-                merged_lists.append(self.merge_two_lists(list1, list2))
-            lists = merged_lists
-        return lists[0]
         
