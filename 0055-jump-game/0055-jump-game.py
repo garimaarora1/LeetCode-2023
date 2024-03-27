4f44060373eb4e1cffaf0bd6eec83b21747832e6
Garima Arora <garimaarora.ga2@gmail.com>
2024-03-27 16:20:38 +0530

    Time: 376 ms (33.90%), Space: 17.8 MB (57.79%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_reachable_index = 0 
+        for i in range(len(nums)):
+            if max_reachable_index < i:
+                return False
+            max_reachable_index = max(max_reachable_index, nums[i]+i)
+        return True
