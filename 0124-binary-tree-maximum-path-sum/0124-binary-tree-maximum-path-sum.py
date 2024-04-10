275707d011bd1d25c6bd5523b6878d2b5ee1063a
Garima Arora <garimaarora.ga2@gmail.com>
2024-04-10 14:02:46 +0530

    Time: 68 ms (35.09%), Space: 21 MB (47.48%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -4,22 +4,22 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        
-        self.maxi = float('-inf')
-        
-        def pre_order(root):
+class Solution(object):
+    maxi = float('-inf')
+    def maxPathSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def find(root):
             if not root:
                 return 0
-            
-            l = pre_order(root.left)
-            r = pre_order(root.right)
-            self.maxi = max(self.maxi, l+r+root.val, root.val, l+root.val, r+root.val)
-            
-            return max(max(l,r) + root.val, root.val)
-            
-        pre_order(root)
+            l = max(0, find(root.left))
+            r = max(0, find(root.right))
+            self.maxi = max(self.maxi, root.val+l+r)
+            return root.val+max(l,r)
+        find(root)
+        return self.maxi
         
         return self.maxi
         
