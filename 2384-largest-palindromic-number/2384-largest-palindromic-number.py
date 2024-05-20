c96bfee74b1bf86b8e434933dd11d9f0ea874f70
Garima Arora <garimaarora.ga2@gmail.com>
2024-05-20 19:31:35 +0530

    Time: 77 ms (50.99%), Space: 18 MB (76.43%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.py
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.py
@@ -2,25 +2,20 @@ class Solution:
     def largestPalindromic(self, num: str) -> str:
         counter = defaultdict(int)
         res = ''
-        mid = 0
+        mid = ''
         for n in num:
             counter[n] += 1
-        sorted_numbers = sorted(list(counter.keys()))
-        sorted_numbers = sorted_numbers[::-1]
+        sorted_numbers = sorted(list(counter.keys()))[::-1]
         for key in sorted_numbers:
+            res += (counter[key]//2) * key
             if counter[key]%2 == 0:
-                res += (counter[key]//2) * key
                 counter[key] = 0
             elif counter[key]-1>0:
-                res += (counter[key]//2) * key
                 counter[key] = 1
         for key in sorted_numbers:
             if counter[key] > 0:
                 mid = key
                 break
-        if mid:
-            res = res + mid + res[::-1]
-        else:
-            res = res + res[::-1]
+        res = res + mid + res[::-1]
         res = res.strip("0")
         return res if res else "0"
