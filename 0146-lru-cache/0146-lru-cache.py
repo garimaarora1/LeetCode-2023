01189a036e2f1030866bc95ede1862353143c427
Garima Arora <garimaarora.ga2@gmail.com>
2024-05-20 17:55:57 +0530

    Time: 570 ms (58.99%), Space: 77.9 MB (30.55%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,6 +1,6 @@
 class Node:
-    def __init__(self, key, value):
-        self.key, self.val = key, value
+    def __init__(self, key, val):
+        self.key, self.val = key, val
         self.prev = self.next = None
 
 class LRUCache:
@@ -8,17 +8,24 @@ class LRUCache:
     def __init__(self, capacity: int):
         self.cap = capacity
         self.cache = {} # pointing to node
-        self.left, self.right = Node(0,0), Node(0,0)
-        self.left.next, self.right.prev = self.right, self.left
-        
+        # dummy nodes
+        self.left = Node(0,0)
+        self.right = Node(0,0)
+        self.left.next = self.right
+        self.right.prev = self.left
+    
     def remove(self, node):
         prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-
+        prev.next = nxt
+        nxt.prev = prev
+        
+    # insert from right 
     def insert(self, node):
         prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev
+        prev.next = node
+        nxt.prev = node
+        node.prev = prev
+        node.next = nxt
 
     def get(self, key: int) -> int:
         if key in self.cache:
@@ -26,7 +33,7 @@ class LRUCache:
             self.insert(self.cache[key])
             return self.cache[key].val
         return -1
-        
+
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
@@ -34,13 +41,12 @@ class LRUCache:
         self.insert(self.cache[key])
         
         if len(self.cache) > self.cap:
+            # remove LRU item from cache 
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
-        
-        
-
-
+    
+    
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
