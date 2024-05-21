e71c56972089f2ebcd9d702d9279b5a6876c78e5
Garima Arora <garimaarora.ga2@gmail.com>
2024-05-21 16:46:27 +0530

    Time: 73 ms (53.77%), Space: 19.7 MB (15.32%) - LeetHub

--- 2998-minimum-number-of-operations-to-make-x-and-y-equal/2998-minimum-number-of-operations-to-make-x-and-y-equal.py
+++ 2998-minimum-number-of-operations-to-make-x-and-y-equal/2998-minimum-number-of-operations-to-make-x-and-y-equal.py
@@ -2,25 +2,24 @@ class Solution:
     def minimumOperationsToMakeEqual(self, x: int, y: int) -> int:
         queue = deque()
         visited = set()
-        queue.append((x,0))
+        queue.append((x, 0))
         visited.add(x)
+        
         while queue:
-            ele, step = queue.popleft()
-            if ele == y:
+            num, step = queue.popleft()
+            if num == y:
                 return step
-            if ele % 11 == 0 and ele//11 not in visited:
-                queue.append((ele//11, step+1))
-                visited.add(ele//11)
-            if ele % 5 == 0 and ele//5 not in visited:
-                queue.append((ele//5, step+1))
-                visited.add(ele//5)
-            if ele-1 not in visited:
-                queue.append((ele-1, step+1))
-                visited.add(ele-1)
-            if ele+1 not in visited:
-                queue.append((ele+1, step+1))
-                visited.add(ele+1)
+            if num % 11 == 0 and num // 11 not in visited:
+                visited.add(num // 11)
+                queue.append((num//11, step+1))
+            if num % 5 ==0 and num //5 not in visited:
+                visited.add(num // 5)
+                queue.append((num//5, step+1))
+            if num - 1 not in visited:
+                visited.add(num-1)
+                queue.append((num-1, step+1))
+            if num + 1 not in visited:
+                visited.add(num+1)
+                queue.append((num+1, step+1))
         return -1
-            
                 
-            
