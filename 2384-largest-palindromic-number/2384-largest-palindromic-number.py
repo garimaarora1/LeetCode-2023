493242637424e590def28eb532416433226af966
Garima Arora <garimaarora.ga2@gmail.com>
2024-05-21 14:01:50 +0530

    Time: 75 ms (53.10%), Space: 17.9 MB (76.43%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.py
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.py
@@ -1,16 +1,28 @@
 class Solution:
     def largestPalindromic(self, num: str) -> str:
+        
+        # 1. hasmap to keep track of freq of each 'int'
+        #     res = ''
+        #     mid = ''
+        # 2. reverse sort the map  -- O(1)
+        # 3. condition %2  freq =0 other freq = 1
+        # 4. condition >=1 
+        # 5. return res + mid + res[::-1]
+    
+    
         counter = defaultdict(int)
-        res = ''
-        mid = ''
+        res, mid = '', ''
         for n in num:
             counter[n] += 1
-        sorted_numbers = sorted(list(counter.keys()))[::-1]
-        for key in sorted_numbers:
-            res += (counter[key]//2) * key
+        sorted_keys = sorted(list(counter.keys()), reverse = True)
+        print(sorted_keys)
+        for key in sorted_keys:
+            res = res + ((key)*((counter[key])//2))
             counter[key] = counter[key]%2
-            if not mid and counter[key] > 0:
+            if counter[key] >=1 and not mid:
                 mid = key
         res = res + mid + res[::-1]
         res = res.strip("0")
         return res if res else "0"
+            
+    
