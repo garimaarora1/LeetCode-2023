09ad515e3af2c46fdb4ef7da5a53f264264df0e6
Garima Arora <garimaarora.ga2@gmail.com>
2024-05-21 20:33:49 +0530

    Time: 66 ms (80.75%), Space: 16.9 MB (33.72%) - LeetHub

--- 1366-rank-teams-by-votes/1366-rank-teams-by-votes.py
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.py
@@ -1,11 +1,20 @@
 class Solution:
     
     def rankTeams(self, votes: List[str]) -> str:
-        d = {ch: [0] * len(votes[0]) for ch in votes[0]} 
-        res = ''
+        def func(second, first):
+            second_array = d[second]
+            first_array = d[first]
+            if second_array > first_array:
+                return -1
+            return 1
+        l = len(votes[0])
+        d = {ch: [0]*l for ch in votes[0]}
         for vote in votes:
-            for rank, ch in enumerate(vote):
-                d[ch][rank] += 1
-        sorted_keys = sorted(d.keys())
-        return "".join(sorted(sorted_keys, key=lambda x: d[x], reverse=True))
+            for i, char in enumerate(vote):
+                d[char][i] += 1
+        sorted_keys = sorted(list(d.keys()))
+        sorted_keys.sort(key=cmp_to_key(func))
+        return ''.join(sorted_keys)
+        
+        
             
