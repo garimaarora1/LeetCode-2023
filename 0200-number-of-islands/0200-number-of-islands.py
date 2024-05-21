801151b229b57b6638e5ac0a8921ad3030914a73
Garima Arora <garimaarora.ga2@gmail.com>
2024-05-21 16:01:56 +0530

    Time: 259 ms (33.85%), Space: 24.2 MB (26.17%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,20 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        def bfs(i, j):
+        def bfs(i,j):
             queue = deque()
             queue.append((i,j))
+            
+            directions = [(0,-1), (0,1), (1,0), (-1,0)]
             while queue:
                 x, y = queue.popleft()
-                directions = [(0,-1), (0,1), (1,0), (-1,0)]
+                
                 for dr,dc in directions:
                     dx = x + dr
                     dy = y + dc
-                    if 0<=dx<row and 0<=dy<col and grid[dx][dy] == '1' and (dx, dy) not in visited:
-                        queue.append((dx, dy))
-                        visited.add((dx, dy))
-            
-        row, col = len(grid), len(grid[0])
+                    
+                    if 0<=dx<row and 0<=dy<col and grid[dx][dy] == '1' and (dx,dy) not in visited:
+                        queue.append((dx,dy))
+                        visited.add((dx,dy))
+                    
+                
+        
+        row = len(grid)
+        col = len(grid[0])
         visited = set()
         count = 0
         for i in range(row):
@@ -24,5 +30,3 @@ class Solution:
                     bfs(i,j)
                     count += 1
         return count
-                    
-    
