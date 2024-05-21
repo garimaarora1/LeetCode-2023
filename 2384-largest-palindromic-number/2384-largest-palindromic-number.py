805244926ac4cf1c369be9df1f47d5b0991cb168
Garima Arora <garimaarora.ga2@gmail.com>
2024-05-21 14:13:15 +0530

    Time: 66 ms (68.50%), Space: 18.1 MB (48.06%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.py
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.py
@@ -15,11 +15,10 @@ class Solution:
         for n in num:
             counter[n] += 1
         sorted_keys = sorted(list(counter.keys()), reverse = True)
-        print(sorted_keys)
         for key in sorted_keys:
-            res = res + ((key)*((counter[key])//2))
-            counter[key] = counter[key]%2
-            if counter[key] >=1 and not mid:
+            res += (counter[key]//2) * key
+            counter[key] = counter[key] % 2
+            if counter[key]>0 and not mid:
                 mid = key
         res = res + mid + res[::-1]
         res = res.strip("0")
