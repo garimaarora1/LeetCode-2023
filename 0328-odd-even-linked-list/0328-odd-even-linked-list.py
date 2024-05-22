bfe7f27fcbc21e2a7bec7744ffad91795249e05b
Garima Arora <garimaarora.ga2@gmail.com>
2024-05-22 12:59:50 +0530

    Time: 44 ms (27.81%), Space: 18.5 MB (74.29%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -0,0 +1,17 @@
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        
+        odd = head
+        even = even_head = head.next
+        
+        while even and even.next:
+            odd.next = odd.next.next
+            even.next = even.next.next
+            odd = odd.next
+            even = even.next
+
+        odd.next = even_head
+
+        return head
