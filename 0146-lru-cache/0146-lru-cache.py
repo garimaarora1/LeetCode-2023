d1ba290ef4f04faf4eb3488c5307b550c861e77d
Garima Arora <garimaarora.ga2@gmail.com>
2024-05-22 19:59:32 +0530

    Time: 593 ms (30.63%), Space: 78.2 MB (14.49%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,33 +1,36 @@
+# class Node for DLL
 class Node:
-    def __init__(self, key, value):
+    def __init__(self, key, val):
         self.key = key
-        self.val = value
-        self.prev = self.next = None
+        self.val = val
+        self.prev, self.next = None, None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.cache = {} # pointing to the nodes
+        self.cache = {} # for pointing to the nodes of DLL
         self.left, self.right = Node(0,0), Node(0,0)
-        self.left.next, self.right.prev = self.right, self.left
+        self.left.next = self.right
+        self.right.prev = self.left
         
     def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-    
+        prev, nextt = node.prev, node.next
+        prev.next = nextt
+        nextt.prev = prev
+
     def insert(self, node):
-        prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev
-    
+        prev, nextt = self.right.prev, self.right
+        prev.next = nextt.prev = node
+        node.next = nextt
+        node.prev = prev
+
     def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
             return self.cache[key].val
         return -1
-        
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
@@ -39,7 +42,6 @@ class LRUCache:
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
-            
         
 
 
