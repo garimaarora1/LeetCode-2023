55b5e1f2344198ecc3cd97a3236d2aa49eb72afe
Garima Arora <garimaarora.ga2@gmail.com>
2024-05-25 01:15:20 +0530

    Time: 125 ms (48.90%), Space: 17.3 MB (39.61%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,47 +1,24 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        """
-        maintain two dicts, 1. for freq of chars of t
-        2. for freq for chars of s
-        
-        maintain two vars, have = 0, need = len(freq_t_dict)
-        two vars for res, res_len
-        
-        loop over s 
-        keep adding elements and count to freq_s_dict
-        if char_freq_t[end] == char_freq_s[end]: have -= 1
-        while have == need:
-        1. update the res
-        2. pop from window from left side
-        """
-        if t == "":
+        d=Counter(t)
+        req=len(d)
+        mini=float('inf')
+        res=[]
+        j=0
+        for i in range(len(s)):
+            if s[i] in d:
+                d[s[i]]-=1
+                if d[s[i]]==0:
+                    req-=1
+            while req==0 and j<len(s):
+                if i-j+1<mini:
+                    res=[j,i]
+                    mini=i-j+1
+                if s[j] in d:
+                    d[s[j]]+=1
+                    if d[s[j]]==1:
+                        req+=1
+                j+=1
+        if mini== float('inf'):
             return ""
-        
-        t_freq_dict, window_freq_dict = dict(), dict()
-        
-        for char in t:
-            t_freq_dict[char] = t_freq_dict.get(char, 0) + 1
-        
-        have, need = 0, len(t_freq_dict)
-        res, res_len = [-1,-1], float("infinity")
-        start = 0
-        for end in range(len(s)):
-            char = s[end]
-            window_freq_dict[char] = window_freq_dict.get(char, 0) + 1
-            
-            if char in t_freq_dict and t_freq_dict[char] == window_freq_dict[char]:
-                have += 1
-            
-            while have == need:
-                # update the res 
-                if end - start + 1 < res_len:
-                    res_len = end - start + 1
-                    res = [start, end]
-                
-                # pop from the left of window
-                window_freq_dict[s[start]] -= 1
-                if s[start] in t_freq_dict and window_freq_dict[s[start]] < t_freq_dict[s[start]]:
-                    have -= 1
-                start += 1
-        start, end = res
-        return s[start:end+1] if res_len != float("infinity") else ""
+        return s[res[0]:res[1]+1]
