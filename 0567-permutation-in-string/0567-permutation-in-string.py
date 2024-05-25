fc4966326803bf2e97ec09911f421846d819df57
Garima Arora <garimaarora.ga2@gmail.com>
2024-05-25 10:03:42 +0530

    Time: 63 ms (55.66%), Space: 16.4 MB (99.62%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -3,24 +3,26 @@ class Solution:
         counter = defaultdict(int)
         for ch in s1:
             counter[ch] += 1
-        window_size = len(counter)
-        i = j = 0
-        count = 0
+        unique_ch = len(counter)
+        i, j = 0, 0
         while j < len(s2):
             ch = s2[j]
             if ch in counter:
                 counter[ch] -= 1
-                
                 if counter[ch] == 0:
-                    window_size -= 1
-                    if window_size == 0:
-                        return True
-            if j - i + 1 == len(s1):
+                    unique_ch -= 1
+                
+                if unique_ch == 0:
+                    return True
+            
+            if j-i+1 == len(s1):
                 if s2[i] in counter:
                     counter[s2[i]] += 1
                     if counter[s2[i]] == 1:
-                        window_size += 1
-                i += 1 
+                        unique_ch += 1
+                    
+                i += 1
             j += 1
-            
         return False
+                
+        
