5301d304b0a49cace9c791a53e952f69a4f7a5d9
Garima Arora <garimaarora.ga2@gmail.com>
2024-05-26 21:57:56 +0530

    Time: 193 ms (45.28%), Space: 18 MB (82.63%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,15 +1,14 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        return self.fun(nums, 0, len(nums)-1, target)
-    
-    def fun(self, nums, low, high, target):
-        if low > high:
-            return -1
-        mid = (low + high)//2
-
-        if nums[mid] == target:
-            return mid
-        if target < nums[mid]:
-            return self.fun(nums, low, mid-1, target)
-        else:
-            return self.fun(nums, mid+1, high, target)
+        low = 0
+        high = len(nums)-1
+        while low <= high:
+            mid = (low+high) // 2
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1       
+        return -1
+        
