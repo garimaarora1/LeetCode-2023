fcc68c9c7ae91d07c5b69499a89cb54465abaa41
Garima Arora <garimaarora.ga2@gmail.com>
2024-05-27 02:46:36 +0530

    Time: 47 ms (42.31%), Space: 19.4 MB (93.26%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -7,17 +7,19 @@
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         res = None
+        
+        # in-order traversal, nodes visited in asc order
         def dfs(root):
             nonlocal k
             nonlocal res
             if not root:
                 return
             dfs(root.left)
+            # decreement k each time a node is visited
             k -=1 
             if k == 0:
                 res = root.val
                 return
             dfs(root.right)
         dfs(root)
-        return res 
-        
+        return res    
