669743353d9f388de88df9c5217c4a3bd8d6b8ef
Garima Arora <garimaarora.ga2@gmail.com>
2024-05-26 12:20:04 +0530

    Time: 28 ms (95.25%), Space: 16.6 MB (29.67%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -0,0 +1,31 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        # counter 
+        # max heap (-ve values )
+        # heapfiy 
+        # prev, res
+        # while prev heap 
+        # prev not heap : return res
+        # cnt, char 
+        # cnt + 1, add char to res 
+        # if prev: push to heap
+        # ic cnt !=0 set prev
+        # return res
+        counter = Counter(s)
+        # max heap
+        heap = [(-cnt, char) for char, cnt in counter.items()]
+        heapq.heapify(heap)
+        prev = None
+        res = ''
+        while heap or prev:
+            if prev and not heap:
+                return ""
+            cnt, char = heapq.heappop(heap)
+            res += char
+            cnt += 1 
+            if prev:
+                heapq.heappush(heap, prev)
+                prev = None
+            if cnt != 0:
+                prev = (cnt, char)
+        return res
