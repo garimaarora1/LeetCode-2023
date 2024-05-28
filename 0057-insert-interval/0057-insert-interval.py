60e4afea2b2d95ad6a12e48cb6ecbaca1508e3ac
Garima Arora <garimaarora.ga2@gmail.com>
2024-05-28 18:25:03 +0530

    Time: 62 ms (98.34%), Space: 19.9 MB (76.41%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,25 +1,23 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        res = []
-        i = 0
-        n = len(intervals)
-        # Case 1: intervals fixed before new interval
-        while i<n and intervals[i][1] < newInterval[0]:
-            res.append(intervals[i])
-            i += 1
+        low, high = 0, len(intervals)-1
+        target = newInterval[0]
+        # binary search 
+        while low <= high:
+            mid = low + (high-low) // 2
+            if intervals[mid][0] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
         
-        # Case 2: overapping intervals
-        # important
-        while i<n and intervals[i][0] <= newInterval[1]:
-            newInterval[0] = min(intervals[i][0], newInterval[0])
-            newInterval[1] = max(intervals[i][1], newInterval[1])
-            i += 1
-        # important
-        res.append(newInterval)
-    
+        # insert
+        intervals.insert(low, newInterval)
         
-        # Case 3: intervals to happesn post new internval
-        while i<n and intervals[i][0] > newInterval[1]:
-            res.append(intervals[i])
-            i += 1
+        # merge intervals
+        res = [intervals[0]]
+        for interval in intervals[1:]:
+            if interval[0] > res[-1][1]:
+                res.append(interval)
+            else:
+                res[-1][1] = max(res[-1][1], interval[1])
         return res
