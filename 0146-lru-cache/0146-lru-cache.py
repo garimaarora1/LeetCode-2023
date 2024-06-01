3854495eb27e15a3d7f07b06d8cead9be7cbd203
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-01 23:35:17 +0530

    Time: 573 ms (54.51%), Space: 78 MB (22.13%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,48 +1,47 @@
-# class Node for DLL
 class Node:
-    def __init__(self, key, val):
+    def __init__(self, key, value):
         self.key = key
-        self.val = val
-        self.prev, self.next = None, None
+        self.value = value
+        self.next = None
+        self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.cache = {} # for pointing to the nodes of DLL
-        self.left, self.right = Node(0,0), Node(0,0)
+        self.cache = {}
+        self.right = Node(0,0)
+        self.left = Node(0,0)
         self.left.next = self.right
         self.right.prev = self.left
         
     def remove(self, node):
-        prev, nextt = node.prev, node.next
-        prev.next = nextt
-        nextt.prev = prev
-
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+    
     def insert(self, node):
-        prev, nextt = self.right.prev, self.right
-        prev.next = nextt.prev = node
-        node.next = nextt
-        node.prev = prev
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
 
     def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
-            return self.cache[key].val
+            return self.cache[key].value
         return -1
+            
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
         self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
-        
         if len(self.cache) > self.cap:
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
-        
 
 
 # Your LRUCache object will be instantiated and called as such:
