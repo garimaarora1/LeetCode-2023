78471f6984e460c700ae2b8188cae687aebc2e46
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-01 16:23:42 +0530

    Time: 1458 ms (84.57%), Space: 55.5 MB (75.89%) - LeetHub

---
+++ 2055-plates-between-candles/2055-plates-between-candles.py
@@ -0,0 +1,45 @@
+class Solution:
+    def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
+        n = len(s)
+        prev_candles = [0] * n
+        next_candles = [0] * n
+        prefix_sum = [0] * n
+        ans = []
+        for i in range(n):
+            if s[i] == '|':
+                prev_candles[i] = i
+            else:       
+                if i == 0:
+                    prev_candles[i] = -1
+                else:
+                    prev_candles[i] = prev_candles[i-1]
+        for i in range(n-1, -1, -1):
+            if s[i] == '|':
+                next_candles[i] = i
+            else:
+                if i == n-1:
+                    next_candles[i] = -1
+                else:
+                    next_candles[i] = next_candles[i+1]
+        for i in range(n):
+            if s[i] == '*':
+                if i == 0:
+                    prefix_sum[i] = 1
+                else:
+                    prefix_sum[i] = prefix_sum[i-1] + 1
+            else:
+                if i != 0:
+                    prefix_sum[i] = prefix_sum[i-1]
+        for query in queries:
+            start = next_candles[query[0]]
+            end = prev_candles[query[1]]
+            if start > query[1] or end < query[0]:
+                ans.append(0)
+                continue
+            ans.append(prefix_sum[end] - prefix_sum[start])
+        return ans
+            
+            
+            
+        
+                
