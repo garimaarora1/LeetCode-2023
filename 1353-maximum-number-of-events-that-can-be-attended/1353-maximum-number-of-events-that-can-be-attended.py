8a736abbeec8b0767a18ebd389d9a704b4fed7e0
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-02 15:40:41 +0530

    Time: 841 ms (58.21%), Space: 65.3 MB (31.34%) - LeetHub

---
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxEvents(self, events: List[List[int]]) -> int:
+        # 1. sort the events based on start time
+        # 2. check what events can be attended today and add those evenet's end time to heap
+        # 3. remove evetns whose end time has gone from the heap
+        # 4. attend the event that can be attended today
+        
+        
+        events.sort()
+        max_days = max(end for start, end in events)
+        curr_day = min(start for start, end in events)
+        i = 0
+        n = len(events)
+        heap = []
+        count = 0
+        
+        while curr_day <= max_days:
+            while i < n and events[i][0] == curr_day:
+                heapq.heappush(heap, events[i][1])
+                i += 1
+            while heap and heap[0] < curr_day:
+                heapq.heappop(heap)
+            
+            if heap:
+                heapq.heappop(heap)
+                count += 1
+            curr_day += 1
+        return count
+        
+        
