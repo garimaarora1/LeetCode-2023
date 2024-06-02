cd3566c53ef27d57d2b5cf95547e48cc90c30817
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-02 16:26:22 +0530

    Time: 649 ms (80.56%), Space: 27.2 MB (70.75%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.py
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -1,29 +1,30 @@
 class Solution:
     def minimumAverageDifference(self, nums: List[int]) -> int:
-        prefix_sum = [0]*len(nums)
-        for i in range(len(nums)):
-            if i == 0:
-                prefix_sum[i] = nums[i]
-            else:
-                prefix_sum[i] = prefix_sum[i-1] + nums[i]
-        
+        curr_prefix_sum = 0
         mini_avg_diff = float('inf')
-        i = 0
-        n = len(nums)-1
+        n = len(nums)
         mini_idx = 0
-        for j in range(len(nums)):
-            beg_prefix_sum = prefix_sum[j]
-            end_prefix_sum = prefix_sum[n] - beg_prefix_sum
-            beg_ele_count = j-i+1
-            end_ele_count = n - beg_ele_count + 1
-            if end_ele_count == 0:
-                end_avg = 0
-            else:
-                end_avg = end_prefix_sum//end_ele_count
-            start_avg = beg_prefix_sum//beg_ele_count
-            if abs(start_avg - end_avg) < mini_avg_diff:
-                mini_avg_diff = min(mini_avg_diff, abs(start_avg - end_avg))
-                mini_idx = j
+        total_sum = sum(nums)
+        
+        for i in range(len(nums)):
+            curr_prefix_sum += nums[i]
+            
+            
+            # Calculate average of left part of array, index 0 to i.
+            left_part_average = curr_prefix_sum
+            left_part_average //= (i + 1)
+            
+            
+            # Calculate average of right part of array, index i+1 to n-1.
+            right_part_average = total_sum - curr_prefix_sum
+            # If right part have 0 elements, then we don't divide by 0.
+            if i != n - 1:
+                right_part_average //= (n - i - 1)
+                
+            abs_diff = abs(left_part_average - right_part_average)
+            if abs_diff < mini_avg_diff:
+                mini_avg_diff = abs_diff
+                mini_idx = i
         return mini_idx
                         
         
