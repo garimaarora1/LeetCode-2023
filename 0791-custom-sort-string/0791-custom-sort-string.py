1aabf2a51aa802a8c7ee08c457ce771bdf917942
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-02 17:47:20 +0530

    Time: 31 ms (80.40%), Space: 16.5 MB (50.19%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -3,10 +3,9 @@ class Solution:
         counter = Counter(s)
         res = ''
         for ch in order:
-            if ch in s:
-                if counter[ch] > 0:
-                    res += ch*counter[ch]
-                    counter[ch] = 0
+            if counter[ch] > 0:
+                res += ch*counter[ch]
+                counter[ch] = 0
         for key in counter:
             if counter[key] > 0:
                 res += key * counter[key]
