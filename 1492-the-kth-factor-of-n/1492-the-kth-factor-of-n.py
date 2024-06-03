4d7d30c220c43589304e23a9330597e0f7fe883b
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-04 00:44:58 +0530

    Time: 32 ms (81.04%), Space: 16.6 MB (20.06%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
@@ -1,8 +1,16 @@
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
-        for i in range(1, n//2+1):
-            if n%i == 0:
+        divisors, sqrt_n = [], int(n**0.5)
+        print(sqrt_n)
+        for x in range(1, sqrt_n + 1):
+            if n % x == 0:
                 k -= 1
+                divisors.append(x)
                 if k == 0:
-                    return i
-        return n if k == 1 else -1
+                    return x
+        
+        if (sqrt_n * sqrt_n == n):
+            k += 1
+                
+        n_div = len(divisors)
+        return n // divisors[n_div - k] if k <= n_div else -1
