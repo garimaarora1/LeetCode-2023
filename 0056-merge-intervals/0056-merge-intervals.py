c3f56b98865c5196101d869b826b7ac0bd536767
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-03 23:56:08 +0530

    Time: 116 ms (87.64%), Space: 20.5 MB (70.57%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,11 +1,22 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key = lambda x: x[0])
-        res = []
-        for interval in intervals:
-            if not res or res[-1][1] < interval[0]:
-                res.append(interval)
-            else:
-                res[-1][1] = max(res[-1][1], interval[1])
-        return res
         
+        """
+        11:51
+        sort by start time
+        """
+        
+        intervals.sort(key=lambda x:x[0])
+        res = []
+        res.append(intervals[0])
+        for interval in intervals[1:]:
+            if res[-1][1] >= interval[0]:
+                res[-1][1] = max(res[-1][1], interval[1])
+            else:
+                res.append(interval)
+        return res
+    
+    
+    
+    
+            
