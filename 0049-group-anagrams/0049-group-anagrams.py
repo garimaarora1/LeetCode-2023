3d71aacc3a26f601cc9f728bf8dab6e78eb44452
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-05 14:34:00 +0530

    Time: 102 ms (17.34%), Space: 22.3 MB (13.82%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagram_dict = defaultdict(list)
+        freq_map = defaultdict(list)
         for word in strs:
-            freq_array = [0] * 26
-            for char in word:
-                freq_array[ord(char)-97] += 1
-            anagram_dict[tuple(freq_array)].append(word)
-        return anagram_dict.values()
+            freq_arr = [0] * 26
+            for ch in word:
+                freq_arr[ord(ch)-97] += 1
+            freq_map[tuple(freq_arr)].append(word)
+        return freq_map.values()
+            
+                
+        
