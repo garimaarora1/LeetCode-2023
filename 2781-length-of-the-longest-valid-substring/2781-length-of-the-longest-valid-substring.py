1985f58ab2a97f2fb662b08889336ac04529bca5
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-05 14:19:59 +0530

    Time: 1455 ms (24.38%), Space: 35.9 MB (52.96%) - LeetHub

--- 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.py
+++ 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.py
@@ -8,6 +8,7 @@ class Solution:
             while k >= max(i, j-10):
                 if word[k:j+1] in forbidden_set:
                     i = k + 1
+                    break
                 k -= 1
             
             max_length = max(max_length, j-i+1)
