3645c18020fe7f45295abc8e5216aef72c80b8b3
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-05 14:19:48 +0530

    Time: 1620 ms (20.53%), Space: 36 MB (47.37%) - LeetHub

--- 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.py
+++ 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.py
@@ -1,7 +1,6 @@
 class Solution:
     def longestValidSubstring(self, word: str, forbidden: List[str]) -> int:
-        i = 0
-        j = 0
+        i, j = 0, 0
         forbidden_set = set(forbidden)
         max_length = 0
         while j < len(word):
@@ -9,8 +8,9 @@ class Solution:
             while k >= max(i, j-10):
                 if word[k:j+1] in forbidden_set:
                     i = k + 1
-                    break
                 k -= 1
+            
             max_length = max(max_length, j-i+1)
             j += 1
         return max_length
+        
