7acccb9ea0fa31af2e483d4307a8f71e0107f8fc
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-05 13:43:34 +0530

    Time: 1511 ms (44.28%), Space: 56.2 MB (28.49%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.py
+++ 2055-plates-between-candles/2055-plates-between-candles.py
@@ -1,41 +1,47 @@
 class Solution:
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
         n = len(s)
-        prev_candles = [-1] * n
-        next_candles = [n] * n
-        prefix_sum = [0] * n
-        ans = []
-        for i in range(n):
+        prev_candle = [-1] * n
+        next_candle = [n] * n
+        prefix_sum_plates = [0] * n
+        res = []
+        
+        # fill prev_candle
+        for i in range(len(s)):
             if s[i] == '|':
-                prev_candles[i] = i
-            else:       
-                if i!=0:
-                    prev_candles[i] = prev_candles[i-1]
-        for i in range(n-1, -1, -1):
-            if s[i] == '|':
-                next_candles[i] = i
-            else:
-                if i != n-1:
-                    next_candles[i] = next_candles[i+1]
-        for i in range(n):
-            if s[i] == '*':
-                if i == 0:
-                    prefix_sum[i] = 1
-                else:
-                    prefix_sum[i] = prefix_sum[i-1] + 1
+                prev_candle[i] = i
             else:
                 if i != 0:
-                    prefix_sum[i] = prefix_sum[i-1]
-        for query in queries:
-            start = next_candles[query[0]]
-            end = prev_candles[query[1]]
-            if start > query[1] or end < query[0]:
-                ans.append(0)
-                continue
-            ans.append(prefix_sum[end] - prefix_sum[start])
-        return ans
-            
-            
-            
+                    prev_candle[i] = prev_candle[i-1]
         
+        # fill next_candle
+        for i in range(n-1,-1,-1):
+            if s[i] == '|':
+                next_candle[i] = i
+            else:
+                if i != n-1:
+                    next_candle[i] = next_candle[i+1] 
+        
+        # fill prefix_sum_plates
+        for i in range(len(s)):
+            if s[i] == '*':
+                if i != 0:
+                    prefix_sum_plates[i] = prefix_sum_plates[i-1] + 1
+                else:
+                    prefix_sum_plates[i] = 1
+            else:
+                if i != 0:
+                    prefix_sum_plates[i] = prefix_sum_plates[i-1]
                 
+                
+        print(prefix_sum_plates)
+        for query in queries:
+            start, end = query[0], query[1]
+            next_candle_idx = next_candle[start]
+            prev_candle_idx = prev_candle[end]
+            
+            if next_candle_idx > end or prev_candle_idx < start:
+                res.append(0)
+            else:
+                res.append(prefix_sum_plates[prev_candle_idx] - prefix_sum_plates[next_candle_idx])
+        return res
