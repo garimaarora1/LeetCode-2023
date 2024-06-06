c79b5e813aa01b9e6430d005d6564b1f570ed99b
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-06 19:50:15 +0530

    Time: 127 ms (14.75%), Space: 16.8 MB (87.83%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,30 +1,36 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        
         def bfs(i,j):
             queue = deque()
             queue.append((i,j))
-            visited.add((i,j))
-            count = 0
-            directions = [(0,-1),(0,1),(1,0),(-1,0)]
+            directions = ((0,1),(1,0),(0,-1),(-1,0))
+            curr_count = 0
             while queue:
-                r,c = queue.popleft()
-                count += 1
-                for x,y in directions:
-                    dx = x+r
-                    dy = y+c
-                    if 0<=dx<row and 0<=dy<col and grid[dx][dy] == 1 and (dx,dy) not in visited:
-                        visited.add((dx,dy))
-                        queue.append((dx,dy))
-                
-            return count  
-                
-        row, col = len(grid), len(grid[0])
+                x, y = queue.popleft()
+                for dr, dc in directions:
+                    dx = x + dr
+                    dy = y + dc
+                    
+                    if 0<=dx<row and 0<=dy<col and grid[dx][dy] == 1 and (dx, dy) not in visited:
+                        curr_count += 1
+                        visited.add((dx, dy))
+                        queue.append((dx, dy))
+            return curr_count
+
+
         visited = set()
-        maxi = 0
+        row = len(grid)
+        col = len(grid[0])
+        res = 0
         for i in range(row):
             for j in range(col):
-                if grid[i][j] == 1 and (i,j) not in visited:
-                    count = bfs(i,j)
-                    maxi = max(maxi, count)
-        return maxi
+                count = 0
+                if grid[i][j] == 1 and grid[i][j] not in visited:
+                    count += 1
+                    visited.add((i,j))
+                    count += bfs(i,j)
+                    res = max(res, count)
+        return res
+        
+        
+        
