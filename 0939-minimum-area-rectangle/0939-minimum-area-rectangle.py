953c483563a20b4e9d9d07ba6fec552346e3bde8
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-09 14:07:07 +0530

    Time: 726 ms (71.70%), Space: 16.9 MB (37.61%) - LeetHub

---
+++ 0939-minimum-area-rectangle/0939-minimum-area-rectangle.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minAreaRect(self, points: List[List[int]]) -> int:
+        min_area = float('inf')
+        points_set = set()
+        for x,y in points:
+            points_set.add((x,y))
+        
+        for x1, y1 in points:
+            for x2, y2 in points:
+                if x2 > x1 and y2 > y1:
+                    if (x1,y2) in points_set and (x2,y1) in points_set:
+                        area = abs(x2-x1) * abs(y2-y1)
+                        if area:
+                            min_area = min(min_area, area)
+        return min_area if min_area != float('inf') else 0
+                            
