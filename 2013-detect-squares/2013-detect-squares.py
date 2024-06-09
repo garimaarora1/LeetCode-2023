19a1b7cbc97e0c2274d33db9365a01d3c021cb52
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-09 15:13:51 +0530

    Time: 257 ms (52.64%), Space: 18.1 MB (92.64%) - LeetHub

---
+++ 2013-detect-squares/2013-detect-squares.py
@@ -0,0 +1,15 @@
+class DetectSquares:
+    def __init__(self):
+        self.cntPoints = Counter()
+
+    def add(self, point: List[int]) -> None:
+        self.cntPoints[tuple(point)] += 1
+
+    def count(self, point: List[int]) -> int:
+        ans = 0
+        x1, y1 = point
+        for (x3, y3), cnt in self.cntPoints.items():
+            if abs(x1 - x3) == 0 or abs(x1 - x3) != abs(y1 - y3):
+                continue  # Skip empty square or invalid square point!
+            ans += cnt * self.cntPoints[(x1, y3)] * self.cntPoints[(x3, y1)]
+        return ans
