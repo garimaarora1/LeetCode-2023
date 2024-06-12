673899f3636ec53242925db3fa857cf804b5876b
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-12 21:15:05 +0530

    Time: 222 ms (33.67%), Space: 18 MB (93.67%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -3,17 +3,17 @@ class Solution:
 
         def bfs(queue):
             visited = set()
+            directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
             while queue:
                 x, y = queue.popleft()
                 visited.add((x, y))
-                directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
                 for dr, dc in directions: 
                     dx, dy = x + dr, y + dc
                     if 0 <= dx < rows and 0 <= dy < cols and (dx, dy) not in visited and matrix[dx][dy] >= matrix[x][y]:
                         queue.append((dx, dy))
             return visited
-    
-    
+
+
         rows, cols = len(matrix), len(matrix[0])
         
         
@@ -25,8 +25,7 @@ class Solution:
         for i in range(cols):
             pacific_queue.append((0, i))
             atlantic_queue.append((rows - 1, i))
-            
-            
+
         pacific_reachable = bfs(pacific_queue)
         atlantic_reachable = bfs(atlantic_queue)
 
