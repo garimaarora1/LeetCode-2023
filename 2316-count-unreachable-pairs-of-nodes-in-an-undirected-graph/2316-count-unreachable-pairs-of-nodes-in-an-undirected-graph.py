b5305d3f08f3452f3ce05037da72c49a1f0c2c62
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-14 00:09:02 +0530

    Time: 1574 ms (89.56%), Space: 77.9 MB (57.93%) - LeetHub

---
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
@@ -0,0 +1,45 @@
+class Solution:
+    def countPairs(self, n: int, edges: List[List[int]]) -> int:
+        
+        '''
+        1. Create adj list
+        2. Initialize count_pairs, remaining_nodes, curr_component_size, visited
+        3. BFS
+        4. count_pairs = curr_component_size * (remaining nodes - curr_comp_size)
+        5. remaining_nodes -= curr_component_size
+        '''
+        
+        # create adj list
+        
+        adj_list = [[] for _ in range(n)]
+        for node1, node2 in edges:
+            adj_list[node1].append(node2)
+            adj_list[node2].append(node1)
+            
+        # intialize vars
+        count_pairs = 0
+        remaining_nodes = n
+        visited = set()
+        curr_component_size = 0
+        
+        def bfs(node):
+            queue = deque()
+            size = 1
+            queue.append(node)
+            visited.add(node)
+            while queue:
+                node = queue.popleft()
+                for adj in adj_list[node]:
+                    if adj not in visited:
+                        size += 1
+                        queue.append(adj)
+                        visited.add(adj)
+            return size
+
+        for i in range(n):
+            if i not in visited:
+                curr_component_size = bfs(i)
+                remaining_nodes -= curr_component_size
+                count_pairs += curr_component_size * (remaining_nodes)
+        return count_pairs
+            
