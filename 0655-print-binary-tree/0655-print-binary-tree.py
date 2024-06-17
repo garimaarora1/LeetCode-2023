78a665dcd48c3def377913ec75bfc69a4e3cf3a1
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-17 19:39:38 +0530

    Time: 43 ms (20.30%), Space: 16.4 MB (82.08%) - LeetHub

--- 0655-print-binary-tree/0655-print-binary-tree.py
+++ 0655-print-binary-tree/0655-print-binary-tree.py
@@ -6,28 +6,28 @@
 #         self.right = right
 class Solution:
     def printTree(self, root: Optional[TreeNode]) -> List[List[str]]:
-
-        def height(root):
+        """
+        row = height
+        cols = 2 ** height - 1
+        """
+        def get_height(root):
             if not root:
                 return 0
-            l = height(root.left)
-            r = height(root.right)
-            return max(l, r) + 1
+            lh = get_height(root.left)
+            rh = get_height(root.right)
+            return max(lh, rh) + 1
         
-        def populate_res(root, low, high, row):
-            if not root:
+        def dfs(node, row, left, right):
+            if not node:
                 return
-            mid = (low + high) // 2
-            res[row][mid] = str(root.val)
-            populate_res(root.left, low, mid-1, row+1)
-            populate_res(root.right, mid+1, high, row+1)
-        
-        row = height(root)
-        cols = (2 **row) - 1
-        res = [[""] * cols for _ in range(row)]
-        
-        populate_res(root, 0, cols, 0)
-        return res
+            mid = (left + right) // 2
+            tree[row][mid] = str(node.val)
+            dfs(node.left, row+1, left, mid-1)
+            dfs(node.right, row+1, mid+1, right)
         
         
-        
+        height = get_height(root)
+        width = (2 ** height) - 1
+        tree = [[''] * width for _ in range(height)]
+        dfs(root, 0, 0, width-1)
+        return tree
