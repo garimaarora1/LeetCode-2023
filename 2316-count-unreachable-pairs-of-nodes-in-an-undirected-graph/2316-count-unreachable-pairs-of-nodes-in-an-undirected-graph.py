7915bfea021164d18ef9d4c841c066f821a3d033
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-17 21:39:27 +0530

    Time: 2500 ms (5.86%), Space: 77.9 MB (43.72%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
@@ -1,28 +1,15 @@
 class Solution:
     def countPairs(self, n: int, edges: List[List[int]]) -> int:
-        
-        '''
-        1. Create adj list
-        2. Initialize count_pairs, remaining_nodes, curr_component_size, visited
-        3. BFS
-        4. remaining_nodes -= curr_component_size
-        5. count_pairs = curr_component_size * remaining_nodes
-        '''
-        
-        # create adj list
-        
-        adj_list = [[] for _ in range(n)]
-        for node1, node2 in edges:
-            adj_list[node1].append(node2)
-            adj_list[node2].append(node1)
-            
-        # intialize vars
-        count_pairs = 0
+        graph = defaultdict(list)
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
         remaining_nodes = n
-        visited = set()
-        curr_component_size = 0
         
-        # bfs
+        
+        curr_nodes = 0
+        unreachable_pairs = 0
+        
         def bfs(node):
             queue = deque()
             size = 1
@@ -30,18 +17,21 @@ class Solution:
             visited.add(node)
             while queue:
                 node = queue.popleft()
-                for adj in adj_list[node]:
+                for adj in graph[node]:
                     if adj not in visited:
                         size += 1
                         queue.append(adj)
                         visited.add(adj)
             return size
-        
-        # iteraring all nodes
+
+        visited = set()    
+
         for i in range(n):
             if i not in visited:
-                curr_component_size = bfs(i)
-                remaining_nodes -= curr_component_size
-                count_pairs += curr_component_size * (remaining_nodes)
-        return count_pairs
-            
+                curr_nodes = bfs(i)
+                remaining_nodes -= curr_nodes
+                unreachable_pairs += curr_nodes * remaining_nodes
+        
+        return unreachable_pairs
+                
+        
