8a26dd84d2b63be28aa2d943017c86128cea311d
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-17 14:17:38 +0530

    Time: 89 ms (60.03%), Space: 18.2 MB (72.78%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,12 +1,15 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
         graph = defaultdict(list)
-        indegree = [0] *  numCourses
+        indegree = [0] * numCourses
+            
         for u,v in prerequisites:
             graph[v].append(u)
             indegree[u] += 1
-            
+        
         queue = deque()
+        
         for i in range(numCourses):
             if indegree[i] == 0:
                 queue.append(i)
@@ -16,8 +19,9 @@ class Solution:
             node = queue.popleft()
             count += 1
             for adj in graph[node]:
-                indegree[adj] -=1 
+                indegree[adj] -= 1
                 if indegree[adj] == 0:
                     queue.append(adj)
-
+            
         return count == numCourses
+        
