73cfffb3d56809e2e95cd3ec2f8132749122b28c
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-17 16:27:59 +0530

    Time: 38 ms (50.28%), Space: 16.5 MB (91.67%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,28 +1,34 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        counter = Counter(s)
-        prev = None
         
-        # max_heap
+        # adding all the values to hash map and store freq for each ch, key: ch, value: freq
+        counter = defaultdict(int)
+        for ch in s:
+            counter[ch] += 1
+        
+        # add each key value pair to a max heap (freq, ch)
         heap = []
         for key, value in counter.items():
             heap.append((-value, key))
         heapq.heapify(heap)
-        res = ""
 
-        while prev or heap:
+        # res, prev
+        res = ''
+        prev = None
+        
+        # loop through the heap
+        while heap or prev:
             if prev and not heap:
                 return ""
             
             freq, ch = heapq.heappop(heap)
             freq += 1
             res += ch
-
             if prev:
                 heapq.heappush(heap, prev)
                 prev = None
-            if freq != 0:
+            if freq!=0:
                 prev = (freq, ch)
+        
+        # return result
         return res
-                
-            
