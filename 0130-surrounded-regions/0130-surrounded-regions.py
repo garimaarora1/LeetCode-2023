795ed9693afe9fe529b17c1e6ae5bbe980d6c587
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-23 03:29:46 +0530

    Time: 126 ms (30.80%), Space: 17.9 MB (71.88%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,33 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        row, col = len(board), len(board[0])
+        
+        def bfs(x, y):
+            queue = deque()
+            queue.append((i,j))
+            visited.add((i,j))
+            directions = [(0,1), (1,0), (0,-1), (-1,0)]
+            while queue:
+                x, y = queue.popleft()
+                for dr, dc in directions:
+                    dx, dy = x+dr, y+dc
+                    if 0<=dx<row and 0<=dy<col and (dx, dy) not in visited and board[dx][dy] == 'O':
+                        queue.append((dx, dy))
+                        visited.add((dx,dy))
+
+                    
+        visited = set()
+        
+        for i in range(row):
+            for j in range(col):
+                if i == 0 or i == row-1 or j == 0 or j == col-1:
+                    if board[i][j] == 'O' and (i,j) not in visited:
+                        bfs(i, j)
+        
+        for i in range(row):
+            for j in range(col):
+                if board[i][j] == 'O' and (i,j) not in visited:
+                    board[i][j] = 'X'
