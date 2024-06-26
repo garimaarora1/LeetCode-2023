aed0308e9ca11046990c8e72219ebaa5feba0a3a
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-27 02:45:12 +0530

    Time: 247 ms (52.18%), Space: 24.1 MB (32.07%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,28 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        
         def bfs(i, j):
             queue = deque()
-            queue.append((i,j))
-            directions = [(0,1),(1,0),(0,-1),(-1,0)]
+            queue.append((i, j))
+            directions = [(1,0), (0,1), (-1,0), (0,-1)]
             while queue:
                 x, y = queue.popleft()
                 for dr, dc in directions:
                     dx = x + dr
                     dy = y + dc
-                    if 0<=dx<row and 0<=dy<col and grid[dx][dy] == '1' and (dx, dy) not in visited:
+                    if 0<=dx<rows and 0<=dy<cols and grid[dx][dy] == '1' and (dx, dy) not in visited:
                         visited.add((dx, dy))
                         queue.append((dx, dy))
-                    
-        row = len(grid)
-        col = len(grid[0])
+        
         visited = set()
         count = 0
-        for i in range(row):
-            for j in range(col):
-                if grid[i][j] == '1' and (i,j) not in visited:
-                    visited.add((i,j))
-                    bfs(i, j)
-                    count += 1
-        return count
-                    
         
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "1" and (i, j) not in visited:
+                    count += 1
+                    bfs(i, j)
+                    visited.add((i, j))
+        return count
