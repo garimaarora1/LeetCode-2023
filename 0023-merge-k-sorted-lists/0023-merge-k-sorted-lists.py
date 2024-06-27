09f1c8ecf3746c30a6ae70f93dbe631524fa44ce
Garima Arora <garimaarora.ga2@gmail.com>
2024-06-27 22:36:35 +0530

    Time: 74 ms (75.97%), Space: 19.3 MB (63.74%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,38 +3,22 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-"""
-
-Approach:
-
-Take the first node of each of the linked lists
-and add it into a heap. When you add it to the heap
-add (node.val, i) where i is the ith list.
-
-Create a dummy node head.
-
-Pop the first node from the heap and make it the
-next node in the dummy-list. Remember to add the
-first node from the ith linked list into the heap
-since we just removed a node from this list from the heap.
-
-Repeat until the heap is empty.
-
-https://leetcode.com/problems/merge-k-sorted-lists/discuss/1032502/Python-Simple-Heap-Solution
-"""
 class Solution:
-        
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        curr = ListNode()
-        head = curr
+        """
+        min heap (val, index_list)
+        curr = heap.pop
+        if that list ist here, heap push
+        """
+        # min_heap
         heap = []
-        # Step 1: iterate over lists and add the first value of each list to heap and move the pointer to next
         for i in range(len(lists)):
             if lists[i]:
                 heapq.heappush(heap, (lists[i].val, i))
                 lists[i] = lists[i].next
-            
-        # Step 2: while heap size is not zero pop the heap and add to curr node.next and refill the heap with the same list if it exist
+        
+        head = ListNode()
+        curr = head
         while heap:
             val, i = heapq.heappop(heap)
             curr.next = ListNode(val)
@@ -42,7 +26,8 @@ class Solution:
             if lists[i]:
                 heapq.heappush(heap, (lists[i].val, i))
                 lists[i] = lists[i].next
-                
-        # Step 3: return head.next
+        
         return head.next
+                
+        
         
