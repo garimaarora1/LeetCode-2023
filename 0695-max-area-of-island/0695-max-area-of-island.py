8529f908efc61f2dafdba3f43660c5599c0fde37
Garima Arora <garimaarora.ga2@gmail.com>
2024-07-02 00:24:29 +0530

    Time: 120 ms (26.97%), Space: 17.9 MB (32.84%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,12 +1,11 @@
 class Solution:
     
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        ones_count = 0 
         row = len(grid)
         col = len(grid[0])
-        max_count = 0
         parent = [i for i in range(row*col)]
-        size = [1 for _ in range(row*col)]
+        size = [1 for _ in range(row*col)] 
+        found = False
         def find(x):
             if parent[x] != x:
                 parent[x] = find(parent[x])
@@ -15,7 +14,6 @@ class Solution:
                 return x
 
         def union(x, y):
-            nonlocal max_count
             x_parent, y_parent = find(x), find(y)
             if x_parent != y_parent:
                 if size[x_parent] < size[y_parent]:
@@ -24,26 +22,19 @@ class Solution:
                 else:
                     parent[y_parent] = x_parent
                     size[x_parent] += size[y_parent]
-            curr_max = max(size[x_parent], size[y_parent])
-            max_count = max(max_count, curr_max)
         
         directions = [(0,1),(1,0),(0,-1),(-1,0)]
         for i in range(row):
             for j in range(col):
                 if grid[i][j] == 1:
+                    found = True
                     position_1 = i*col + j 
                     for dx, dy in directions:
                         x = i + dx
                         y = j + dy
                         if 0<=x<row and 0<=y<col and grid[x][y] == 1:
                             position_2 = x*col + y
-                            union(position_2, position_1)
-        max_area = 0
-        found = False
-        for r in range(row):
-            for c in range(col):
-                if grid[r][c] == 1:
-                    found = True
+                            union(position_2, position_1)         
         if found:
             return max(size)
         return 0
