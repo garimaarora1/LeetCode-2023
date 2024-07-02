3e645182095c6e580f1e6aa4308a9995888f1c57
Garima Arora <garimaarora.ga2@gmail.com>
2024-07-02 22:36:30 +0530

    Time: 1621 ms (55.75%), Space: 56.4 MB (69.55%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.py
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.py
@@ -16,6 +16,8 @@ class Solution:
                 return True
             return False
         
+        a_count = 1
+        b_count = 1
         redundant_edges = 0
         a_parent = [i for i in range(n+1)]
         b_parent = [i for i in range(n+1)]
@@ -23,31 +25,24 @@ class Solution:
         
         for typ,u,v in edges:
             if typ == 3:
-                if not union(u, v, a_parent):
+                if union(u, v, a_parent):
+                    a_count += 1
+                    b_count += 1
+                else:
                     redundant_edges += 1
                 union(u,v, b_parent)
             elif typ == 2:
-                if not union(u, v, b_parent):
+                if union(u, v, b_parent):
+                    b_count += 1
+                else:
                     redundant_edges += 1
             else:
-                if not union(u, v, a_parent):
+                if union(u, v, a_parent):
+                    a_count += 1
+                else:
                     redundant_edges += 1
 
-        count = 0
-        for i in range(1, len(a_parent)):
-            if i == a_parent[i]:
-                count += 1
-            if count == 2:
-                return -1
-        count = 0
-        for i in range(1, len(b_parent)):
-            if i == b_parent[i]:
-                count += 1
-            if count == 2:
-                return -1
-        
+        if a_count < n or b_count < n:
+            return - 1
         return redundant_edges
-        
-        
-            
-                    
+ 
