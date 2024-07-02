bc7cd8c4b60ad5aae60819b542dca8d92ec6a64f
Garima Arora <garimaarora.ga2@gmail.com>
2024-07-02 22:19:36 +0530

    Time: 1643 ms (46.13%), Space: 56.2 MB (86.98%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.py
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.py
@@ -16,8 +16,7 @@ class Solution:
                 return True
             return False
         
-        a_count = 0
-        b_count = 0
+        redundant_edges = 0
         a_parent = [i for i in range(n+1)]
         b_parent = [i for i in range(n+1)]
         edges = sorted(edges, key=lambda x:-x[0])
@@ -25,14 +24,14 @@ class Solution:
         for typ,u,v in edges:
             if typ == 3:
                 if not union(u, v, a_parent):
-                    a_count += 1
+                    redundant_edges += 1
                 union(u,v, b_parent)
             elif typ == 2:
                 if not union(u, v, b_parent):
-                    b_count += 1
+                    redundant_edges += 1
             else:
                 if not union(u, v, a_parent):
-                    a_count += 1
+                    redundant_edges += 1
 
         count = 0
         for i in range(1, len(a_parent)):
@@ -47,7 +46,7 @@ class Solution:
             if count == 2:
                 return -1
         
-        return a_count + b_count
+        return redundant_edges
         
         
             
