135ffa53993c639409e6e93a052fe7ab74842401
Garima Arora <garimaarora.ga2@gmail.com>
2024-07-02 22:17:20 +0530

    Time: 1615 ms (59.30%), Space: 56.3 MB (69.55%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.py
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.py
@@ -1,61 +1,64 @@
 class Solution:
     def maxNumEdgesToRemove(self, n: int, edges: List[List[int]]) -> int:
         
-        #    int find(int u) {
-        # if (u != parent[u])
-        #     parent[u] = find(parent[u]);
-        # return parent[u];
+        # def find(x, parent):
+        #     if parent[x] == x:
+        #         return x
+        #     else:
+        #         temp_parent = find(parent[x], parent)
+        #         parent[x] = temp_parent
+        #         return temp_parent
+        
         def find(x, parent):
             if parent[x] != x:
                 parent[x] = find(parent[x], parent)
             return parent[x]
-                
-                # return find(parent[x], parent)
-        
-        def union(u, v, parent):
-            rep_u = find(u, parent)
-            rep_v = find(v, parent)
-            if rep_u != rep_v:
-                if rep_u > rep_v:
-                    parent[rep_v] = rep_u
+
+        def union(x, y, parent):
+            parent_x = find(x, parent)
+            parent_y = find(y, parent)
+            if parent_x != parent_y:
+                if parent_x < parent_y:
+                    parent[parent_y] = parent_x
                 else:
-                    parent[rep_u] = rep_v
+                    parent[parent_x] = parent_y
                 return True
+            
             return False
         
-        
+        a_count = 0
+        b_count = 0
+        a_parent = [i for i in range(n+1)]
+        b_parent = [i for i in range(n+1)]
         edges = sorted(edges, key=lambda x:-x[0])
-        alice_parents = [i for i in range(n+1)]
-        bob_parents = [i for i in range(n+1)]
-        alice_count, bob_count = 0, 0
-        for typ, u, v in edges:
+        
+        for typ,u,v in edges:
             if typ == 3:
-                if not union(u, v, alice_parents):
-                    alice_count += 1
-                union(u, v, bob_parents)
+                if not union(u, v, a_parent):
+                    a_count += 1
+                union(u,v, b_parent)
             elif typ == 2:
-                if not union(u, v, bob_parents):
-                    bob_count += 1
+                if not union(u, v, b_parent):
+                    b_count += 1
             else:
-                if not union(u, v, alice_parents):
-                    alice_count += 1
+                if not union(u, v, a_parent):
+                    a_count += 1
 
         count = 0
-        for i in range(1, len(alice_parents)):
-            if i == alice_parents[i]:
+        for i in range(1, len(a_parent)):
+            if i == a_parent[i]:
                 count += 1
             if count == 2:
                 return -1
         count = 0
-        for i in range(1, len(bob_parents)):
-            if i == bob_parents[i]:
+        for i in range(1, len(b_parent)):
+            if i == b_parent[i]:
                 count += 1
             if count == 2:
                 return -1
-        return alice_count + bob_count
         
-                
-                
+        return a_count + b_count
+        
+        
             
-            
-                
+                    
