29765f07f80b234206344c1ed0bc4fd4f0c254e6
Garima Arora <garimaarora.ga2@gmail.com>
2024-07-02 22:47:31 +0530

    Time: 1590 ms (76.61%), Space: 56.3 MB (69.55%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.py
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.py
@@ -5,49 +5,49 @@ class Solution:
                 parent[x] = find(parent[x], parent)
             return parent[x]
 
-        def union(x, y, parent, rank):
+        def union(x, y, parent):
             parent_x = find(x, parent)
             parent_y = find(y, parent)
             if parent_x != parent_y:
-                if rank[parent_x] > rank[parent_y]:
+                if parent_x < parent_y:
                     parent[parent_y] = parent_x
-                elif rank[parent_x] < rank[parent_y]:
-                    parent[parent_x] = parent_y
                 else:
                     parent[parent_x] = parent_y
-                    rank[parent_y] += 1
                 return True
             return False
         
-        a_count = 1
-        b_count = 1
         redundant_edges = 0
         a_parent = [i for i in range(n+1)]
-        a_rank = [1 for _ in range(n+1)]
         b_parent = [i for i in range(n+1)]
-        b_rank = [1 for _ in range(n+1)]
         edges = sorted(edges, key=lambda x:-x[0])
         
         for typ,u,v in edges:
             if typ == 3:
-                if union(u, v, a_parent, a_rank):
-                    a_count += 1
-                    b_count += 1
-                else:
+                if not union(u, v, a_parent):
                     redundant_edges += 1
-                union(u,v, b_parent, b_rank)
+                union(u,v, b_parent)
             elif typ == 2:
-                if union(u, v, b_parent, b_rank):
-                    b_count += 1
-                else:
+                if not union(u, v, b_parent):
                     redundant_edges += 1
             else:
-                if union(u, v, a_parent, a_rank):
-                    a_count += 1
-                else:
+                if not union(u, v, a_parent):
                     redundant_edges += 1
 
-        if a_count < n or b_count < n:
-            return - 1
+        count = 0
+        for i in range(1, len(a_parent)):
+            if i == a_parent[i]:
+                count += 1
+            if count == 2:
+                return -1
+        count = 0
+        for i in range(1, len(b_parent)):
+            if i == b_parent[i]:
+                count += 1
+            if count == 2:
+                return -1
+        
         return redundant_edges
- 
+        
+        
+            
+                    
