fdb333569eac7e0dbf59922aa23278b78b3acf85
Garima Arora <garimaarora.ga2@gmail.com>
2024-07-04 01:42:23 +0530

    Time: 82 ms (22.75%), Space: 16.8 MB (80.07%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        tickets.sort()
+        graph = defaultdict(list)
+        for u, v in tickets:
+            graph[u].append(v)
+        
+        res =  []
+        def dfs(source):
+            while graph[source]:
+                node = graph[source].pop(0)
+                dfs(node)
+            res.append(source)
+                
+        source = "JFK"
+        dfs(source)
+        return res[::-1]
