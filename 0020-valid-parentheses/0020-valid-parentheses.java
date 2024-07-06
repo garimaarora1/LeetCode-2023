fb527689f0f7e728bf47973f760270232be982f0
Garima Arora <garimaarora.ga2@gmail.com>
2024-07-07 01:29:58 +0530

    Time: 2 ms (54.53%), Space: 41.4 MB (54.44%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,30 +1,32 @@
 class Solution {
     private static final HashMap<Character, Character> MAPPINGS;
-        
+    
     static {
         MAPPINGS = new HashMap<>();
         MAPPINGS.put(']', '[');
         MAPPINGS.put('}', '{');
         MAPPINGS.put(')', '(');
     }
-
+    
+    
     public boolean isValid(String s) {
-
         Stack<Character> stack = new Stack<>();
+        
         for (char ch : s.toCharArray()) {
-
-            if(MAPPINGS.containsKey(ch)) {
+            
+            if (MAPPINGS.containsKey(ch)) {
                 char topElement = stack.isEmpty() ? '#' : stack.pop();
 
-                if (topElement != MAPPINGS.get(ch)) {
+                if (topElement != MAPPINGS.get(ch))
                     return false;
-                }
             } else {
                 stack.push(ch);
             }
-        }
-
+            
+        } 
+        
         return stack.isEmpty();
+        
         
     }
 }
