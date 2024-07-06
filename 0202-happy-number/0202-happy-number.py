7077bc08fbaba51a83e4fd5971f9183b3c8189ee
Garima Arora <garimaarora.ga2@gmail.com>
2024-07-06 20:09:43 +0530

    Time: 43 ms (19.05%), Space: 16.7 MB (5.73%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def get_next_number(self, n):
+        total_sum = 0
+        while n > 0:
+            last_digit = n % 10
+            n = n // 10
+            total_sum += last_digit**2
+        return total_sum
+        # self.get_next_number(n, total_sum)
+        
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while n!=1 and n not in seen:
+            seen.add(n)
+            n = self.get_next_number(n)
+        
+        return True if n == 1 else False
