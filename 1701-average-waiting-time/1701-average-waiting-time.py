59f5f67fa2b8abfb677afb658d9a8bfc9be10b2b
Garima Arora <garimaarora.ga2@gmail.com>
2024-07-09 15:44:47 +0530

    Time: 774 ms (21.31%), Space: 57.1 MB (72.85%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.py
+++ 1701-average-waiting-time/1701-average-waiting-time.py
@@ -5,6 +5,5 @@ class Solution:
         for curr_start_time, curr_prep_time in customers:
             next_idle_time = max(curr_start_time, next_idle_time) + curr_prep_time
             curr_waiting_time = next_idle_time - curr_start_time
-            
             total_waiting_time += curr_waiting_time
         return total_waiting_time/len(customers)
