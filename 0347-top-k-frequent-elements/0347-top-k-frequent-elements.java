a8d741d754ba697a6d639820614b62935823d738
Garima Arora <garimaarora.ga2@gmail.com>
2024-07-12 03:26:39 +0530

    Time: 15 ms (38.86%), Space: 48.9 MB (12.01%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        // O(1) time
+        if (k == nums.length) {
+            return nums;
+        }
+        
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        
+        // 1. Build hash map: character and how often it appears
+        // O(N) time
+        for(int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+        
+        // init heap 'the less frequent element first'
+        
+        Queue<Integer> heap = new PriorityQueue<>((n1, n2) -> freqMap.get(n1) - freqMap.get(n2));
+        
+        // 2. Keep k top frequent elements in the heap
+        // O(N log k) < O(N log N) time
+        for (int n: freqMap.keySet()) {
+          heap.add(n);
+          if (heap.size() > k) heap.poll();    
+        }
+        
+        // 3. Build an output array
+        // O(k log k) time
+        int[] top = new int[k];
+        for(int i = k - 1; i >= 0; --i) {
+            top[i] = heap.poll();
+        }
+        return top;
+    }
+}
