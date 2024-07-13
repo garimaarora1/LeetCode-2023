b0d9080ead9f2f2e9c0cb9fd5830f82aa15402db
Garima Arora <garimaarora.ga2@gmail.com>
2024-07-13 23:25:52 +0530

    Time: 1048 ms (29.81%), Space: 45.7 MB (36.54%) - LeetHub

---
+++ 2751-robot-collisions/2751-robot-collisions.py
@@ -0,0 +1,22 @@
+class Solution:
+    def survivedRobotsHealths(self, positions: List[int], healths: List[int], directions: str) -> List[int]:
+        sorted_pos = [(pos,i) for i,pos in enumerate(positions)]
+        sorted_pos.sort()
+        stack = []
+        for pos, i in sorted_pos:
+            if directions[i] == 'R':
+                stack.append([i, healths[i]])
+            else:
+                while stack and  directions[stack[-1][0]] == 'R' and stack[-1][1] < healths[i]:
+                    stack.pop()
+                    healths[i] -= 1
+                if stack and directions[stack[-1][0]] == 'R' and stack[-1][1] > healths[i]:
+                    stack[-1][1] -= 1
+                    continue
+                if not stack or (directions[stack[-1][0]] == 'L'):
+                    stack.append([i, healths[i]])
+                if directions[stack[-1][0]] == 'R' and stack[-1][1] == healths[i]:
+                    stack.pop()
+        
+        stack.sort()
+        return [health for i, health in stack]
