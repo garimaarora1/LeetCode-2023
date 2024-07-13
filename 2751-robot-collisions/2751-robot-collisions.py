eb1f45219791cc8894f9da7ccf34500488eac764
Garima Arora <garimaarora.ga2@gmail.com>
2024-07-13 23:30:45 +0530

    Time: 1034 ms (31.73%), Space: 44.8 MB (46.15%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.py
+++ 2751-robot-collisions/2751-robot-collisions.py
@@ -1,22 +1,26 @@
 class Solution:
     def survivedRobotsHealths(self, positions: List[int], healths: List[int], directions: str) -> List[int]:
-        sorted_pos = [(pos, i) for i, pos in enumerate(positions)]
-        sorted_pos.sort()
+        # Pair positions with their indices and sort based on positions
+        sorted_pos = sorted((pos, i) for i, pos in enumerate(positions))
         stack = []
-        for pos, i in sorted_pos:
+        
+        for _, i in sorted_pos:
             if directions[i] == 'R':
                 stack.append([i, healths[i]])
             else:
-                while stack and  directions[stack[-1][0]] == 'R' and stack[-1][1] < healths[i]:
-                    stack.pop()
-                    healths[i] -= 1
-                if stack and directions[stack[-1][0]] == 'R' and stack[-1][1] > healths[i]:
-                    stack[-1][1] -= 1
-                    continue
-                if not stack or (directions[stack[-1][0]] == 'L'):
+                while stack and directions[stack[-1][0]] == 'R':
+                    if stack[-1][1] > healths[i]:
+                        stack[-1][1] -= 1
+                        break
+                    elif stack[-1][1] < healths[i]:
+                        healths[i] -= 1
+                        stack.pop()
+                    else:
+                        stack.pop()
+                        break
+                else:
                     stack.append([i, healths[i]])
-                if directions[stack[-1][0]] == 'R' and stack[-1][1] == healths[i]:
-                    stack.pop()
         
-        stack.sort()
-        return [health for i, health in stack]
+        # Sort the stack based on the original indices and return the healths
+        stack.sort(key=lambda x: x[0])
+        return [health for _, health in stack]
