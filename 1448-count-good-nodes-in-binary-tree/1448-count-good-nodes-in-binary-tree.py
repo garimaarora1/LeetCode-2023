f3e49505e45650db282d28c01f9c94986fd44be2
Garima Arora <garimaarora.ga2@gmail.com>
2024-07-16 03:09:58 +0530

    Time: 143 ms (27.87%), Space: 31.1 MB (65.84%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -4,20 +4,18 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# find the count of nodes greater than or equal to the root node value 
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        def pre_order(root, max_so_far):
-            nonlocal c
-            if max_so_far <= root.val:
-                c += 1
-            if root.left:
-                pre_order(root.left, max(root.val, max_so_far))
-            if root.right:
-                pre_order(root.right, max(root.val, max_so_far))
-        c = 0
-        pre_order(root, float('-inf'))
-        return c
-        
-        
-        
+        def pre_order_traversal(root, max_so_far):
+            if not root:
+                return
+            nonlocal count
+            
+            if root.val >= max_so_far:
+                count += 1
+            pre_order_traversal(root.left, max(max_so_far, root.val))
+            pre_order_traversal(root.right, max(max_so_far, root.val))
+                    
+        count = 0
+        pre_order_traversal(root, float('-inf'))
+        return count
