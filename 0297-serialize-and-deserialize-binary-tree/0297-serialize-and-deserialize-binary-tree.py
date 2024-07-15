136a2731804cba73e510ae424fbbda29b01e43ff
Garima Arora <garimaarora.ga2@gmail.com>
2024-07-16 03:55:49 +0530

    Time: 82 ms (59.47%), Space: 20.7 MB (19.97%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -13,18 +13,19 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        res = []
-        def dfs(root):
-            if not root:
-                res.append('N')
+        def dfs(node):
+            if not node:
+                res.append("N")
+                # important: return
                 return
-            res.append(str(root.val))
-            dfs(root.left)
-            dfs(root.right)
-        
+            # important: str
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        res = []
         dfs(root)
         return ','.join(res)
-
+        
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -40,13 +41,11 @@ class Codec:
             node = TreeNode(values[self.i])
             node.left = dfs()
             node.right = dfs()
-            
             return node
-        
+        # important: self
         self.i = -1
         values = data.split(',')
         root = dfs()
-        
         return root
         
 
