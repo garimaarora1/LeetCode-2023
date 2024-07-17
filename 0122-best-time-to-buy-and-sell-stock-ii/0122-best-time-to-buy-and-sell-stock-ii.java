835fc3c26c86d6dc645e99d40bd9618d93e08ca1
Garima Arora <garimaarora.ga2@gmail.com>
2024-07-17 23:44:35 +0530

    Time: 1 ms (89.98%), Space: 45.3 MB (86.56%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buyPrice = prices[0];
+        int maxProfit = 0;
+        int n = prices.length;
+        for(int i = 1; i < n; i++){
+            if (prices[i] < buyPrice) buyPrice = prices[i];
+            else {
+                maxProfit += prices[i] - buyPrice;
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+}
