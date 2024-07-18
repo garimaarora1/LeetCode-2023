030573fb12e8d8fbf2139ed204045ca40113e94a
Garima Arora <garimaarora.ga2@gmail.com>
2024-07-18 22:17:17 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (69.13%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -12,17 +12,16 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
         
-        if (head == null) return false;
+        if (head == null || head.next == null) return false;
         
         ListNode fast = head.next;
         ListNode slow = head;
-        
-        while(slow != fast) {
+        while (fast != slow) {
             if (fast == null || fast.next == null) return false;
             fast = fast.next.next;
             slow = slow.next;
         }
-        return true;
         
+        return true;
     }
 }
