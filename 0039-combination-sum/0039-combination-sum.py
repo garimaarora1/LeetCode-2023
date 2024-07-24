67cdfc25726aceb02ecd888f41c3b159eb5e4957
Garima Arora <garimaarora.ga2@gmail.com>
2024-07-24 20:44:06 +0530

    Time: 52 ms (61.05%), Space: 16.5 MB (61.81%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,17 +1,25 @@
-class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
-        def dfs(i, curr, total):
-            if total == target:
-                res.append(curr.copy())
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        def backtrack(i, subset,current_sum):
+            # base condition
+            if current_sum == target:
+                res.append(subset[:])
                 return
-            if i >= len(candidates) or total > target:
-                return
-            
-            curr.append(candidates[i])
-            dfs(i, curr, total + candidates[i])
-            curr.pop()
-            dfs(i+1, curr, total)
-        
-        dfs(0, [] , 0)
+            if i == len(candidates):
+                return 
+            for j in range(i,len(candidates)):
+                if current_sum + candidates[j] > target:
+                    continue
+                subset.append(candidates[j])
+                # choose as many timesss
+                backtrack(j, subset, current_sum + candidates[j])
+                subset.pop()  
+                
+        res=[]
+        backtrack(0, [], 0)
         return res
