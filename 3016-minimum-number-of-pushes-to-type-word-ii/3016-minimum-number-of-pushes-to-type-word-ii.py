3195d95f236f8cd398743045942ce7bbbbbb6e37
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-06 05:53:20 +0530

    Time: 141 ms (87.11%), Space: 17.7 MB (36.75%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -1,32 +1,13 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        
-        """
-        word 
-        8 keys
-        26 characters
-        
-        3*6 = 18 
-        4*2 = 8: 
-        
-        freq: counter(word)
-        
-        get values in reverse sorted order 
-        
-        key  = 0
-        row = 0
-        
-        if key == 9:
-        row += 1
-        coutn += row * freq
-        """
         freq_map = Counter(word)
-        sorted_freq = sorted(freq_map.values(), reverse=True)
+        heap = [-freq for freq in freq_map.values()]
+        heapq.heapify(heap)
         key, row = 0, 0
         min_count = 0
-        for freq in sorted_freq:
+        while heap:
             row = (key // 8) + 1
-            min_count += row * freq
+            min_count += row * (-heapq.heappop(heap))
             key += 1
         return min_count
             
