c451b859725a3ddb15edf0b431e95d06178eed1a
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-08 21:09:09 +0530

    Time: 74 ms (76.54%), Space: 19.3 MB (68.06%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,48 +3,26 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-"""
-
-Approach:
-
-Take the first node of each of the linked lists
-and add it into a heap. When you add it to the heap
-add (node.val, i) where i is the ith list.
-
-Create a dummy node head.
-
-Pop the first node from the heap and make it the
-next node in the dummy-list. Remember to add the
-first node from the ith linked list into the heap
-since we just removed a node from this list from the heap.
-
-Repeat until the heap is empty.
-
-https://leetcode.com/problems/merge-k-sorted-lists/discuss/1032502/Python-Simple-Heap-Solution
-"""
-class Solution:
+class Solution(object):
+    def mergeKLists(self, lists):
+        if not lists:
+            return None
+        if len(lists) == 1:
+            return lists[0]
+        mid = len(lists) // 2
         
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        curr = ListNode()
-        head = curr
-        heap = []
-        # Step 1: iterate over lists and add the first value of each list to heap and move the pointer to next
-        for i in range(len(lists)):
-            if lists[i]:
-                heapq.heappush(heap, (lists[i].val, i))
-                lists[i] = lists[i].next
-            
-        # Step 2: while heap size is not zero pop the heap and add to curr node.next and refill the heap with the same list if it exist
-        while heap:
-            val, i = heapq.heappop(heap)
-            curr.next = ListNode(val)
-            
-            # Important
-            curr = curr.next
-            if lists[i]:
-                heapq.heappush(heap, (lists[i].val, i))
-                lists[i] = lists[i].next
-                
-        # Step 3: return head.next
-        return head.next
-        
+        l, r = self.mergeKLists(lists[:mid]), self.mergeKLists(lists[mid:])
+        return self.merge(l, r)
+    
+    def merge(self, l, r):
+        dummy = p = ListNode()
+        while l and r:
+            if l.val < r.val:
+                p.next = l
+                l = l.next
+            else:
+                p.next = r
+                r = r.next
+            p = p.next
+        p.next = l or r
+        return dummy.next
