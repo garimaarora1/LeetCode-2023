c68f835660b55fa72086bc5629df23be1a623f3f
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-14 02:17:14 +0530

    Time: 48 ms (11.45%), Space: 16.8 MB (27.85%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        curr_ans = []
+        n = len(nums)
+        def dfs(i, curr_ans):
+            # print(curr_ans)
+            if len(curr_ans) == n:
+                ans.append(curr_ans.copy())
+                return
+
+            for j in range(i, n):
+                curr_ans.append(nums[j])
+                nums[i], nums[j] = nums[j], nums[i]
+                dfs(i+1, curr_ans)
+                curr_ans.pop()
+                nums[i], nums[j] = nums[j], nums[i]
+        
+        dfs(0, curr_ans)
+        return ans
