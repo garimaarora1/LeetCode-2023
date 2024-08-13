2a9f687908521d4d380c6241d07bcfaedcc84e50
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-14 01:09:58 +0530

    Time: 43 ms (17.20%), Space: 16.6 MB (60.71%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,19 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # 1. order has to be maintained 
-        # 2. total 2**n -1
-        # 3. TC: 2**n
-        def dfs(idx, path):
-            if idx == len(nums):
-                res.append(path.copy())
-                return
-            path.append(nums[idx])
-            dfs(idx+1, path)
-            path.pop()
+        ans = []
+        curr_ans = []
+        n = len(nums)
+        def dfs(i, curr_ans):
+            ans.append(curr_ans.copy())
             
-            dfs(idx+1, path)
-            
-        res = []
-        path = []
-        dfs(0, path)
-        return res
+            for j in range(i, n):
+                curr_ans.append(nums[j])
+                dfs(j+1, curr_ans)
+                curr_ans.pop()
+        dfs(0, curr_ans)
+        
+        return ans
+        
+        
