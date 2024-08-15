61c62fe7ca5154266b271fcf756e5343a5b6d89b
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-15 19:01:41 +0530

    Time: 111 ms (85.66%), Space: 31.4 MB (35.95%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,6 +1,7 @@
 class TrieNode:
+    
     def __init__(self):
-        self.children = {}
+        self.children = defaultdict(TrieNode)
         self.end_of_word = False
 
 class Trie:
@@ -11,28 +12,29 @@ class Trie:
 
     def insert(self, word: str) -> None:
         curr = self.root
-        for char in word:
-            if char not in curr.children:
-                curr.children[char] = TrieNode()
-            curr = curr.children[char]
+        for ch in word:
+            if ch not in curr.children:
+                curr.children[ch] = TrieNode()
+            curr = curr.children[ch]
         curr.end_of_word = True
+            
         
 
     def search(self, word: str) -> bool:
         curr = self.root
-        for char in word:
-            if char not in curr.children:
+        for ch in word:
+            if ch not in curr.children:
                 return False
-            curr = curr.children[char]
-        return curr.end_of_word
+            curr = curr.children[ch]
+        return curr.end_of_word == True
         
 
     def startsWith(self, prefix: str) -> bool:
         curr = self.root
-        for char in prefix:
-            if char not in curr.children:
+        for ch in prefix:
+            if ch not in curr.children:
                 return False
-            curr = curr.children[char]
+            curr = curr.children[ch]
         return True
         
 
