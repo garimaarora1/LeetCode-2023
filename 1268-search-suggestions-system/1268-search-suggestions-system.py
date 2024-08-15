a95ce8ca030a64b64ff6cb8a370991c7820e6f1c
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-16 03:18:34 +0530

    Time: 348 ms (19.26%), Space: 22.9 MB (38.14%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,24 +1,41 @@
+class TrieNode:
+    def __init__(self):
+        self.word = None
+        self.child = defaultdict(TrieNode)
+
+    def addWord(self, word):
+        curr = self
+        for c in word:
+            curr = curr.child[c]
+        curr.word = word
+
+    def getWords(self, limit):
+        words = []
+
+        def dfs(curr):
+            if len(words) == limit: return
+            if curr.word != None:
+                words.append(curr.word)
+            for c in sorted(curr.child.keys()):
+                if c in curr.child:
+                    dfs(curr.child[c])
+
+        dfs(self)
+        return words
+
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        class TrieNode:
-            def __init__(self):
-                self.children = collections.defaultdict(TrieNode)
-                self.suggestion = []
-            
-            def add_sugestion(self, product):
-                if len(self.suggestion) < 3:
-                    self.suggestion.append(product)
-        
-        products = sorted(products)
         root = TrieNode()
-        for p in products:
-            node = root
-            for char in p:
-                node = node.children[char]
-                node.add_sugestion(p)
-        
-        result, node = [], root
-        for char in searchWord:
-            node = node.children[char]
-            result.append(node.suggestion)
-        return result
+        for product in products:
+            root.addWord(product)
+
+        ans = []
+        curr = root
+        for c in searchWord:
+            if curr != None and c in curr.child:
+                curr = curr.child[c]
+                ans.append(curr.getWords(3))
+            else:
+                curr = None
+                ans.append([])
+        return ans
