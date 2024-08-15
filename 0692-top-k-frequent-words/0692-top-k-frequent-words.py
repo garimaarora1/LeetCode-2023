316dd2bd743281479d6a4e43b8f0fc768e3e64f5
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-16 01:16:19 +0530

    Time: 79 ms (5.06%), Space: 16.8 MB (30.56%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -0,0 +1,42 @@
+from collections import Counter
+
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        n = len(words)
+        cnt = Counter(words)
+        bucket = [{} for _ in range(n+1)]
+        self.k = k
+
+        def add_word(trie: Mapping, word: str) -> None:
+            root = trie
+            for c in word:
+                if c not in root:
+                    root[c] = {}
+                root = root[c]
+            root['#'] = {}
+
+        def get_words(trie: Mapping, prefix: str) -> List[str]:
+            if self.k == 0:
+                return []
+            res = []
+            if '#' in trie:
+                self.k -= 1
+                print(prefix)
+                res.append(prefix)
+            for i in range(26):
+                c = chr(ord('a') + i)
+                if c in trie:
+                    res += get_words(trie[c], prefix+c)
+            return res
+
+        for word, freq in cnt.items():
+            add_word(bucket[freq], word)
+
+        res = []
+        for i in range(n, 0, -1):
+            if self.k == 0:
+                return res
+            if bucket[i]:
+                res += get_words(bucket[i], '')
+        return res
