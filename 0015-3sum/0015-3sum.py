8625c64b2dbe1bac0f54d6ffa3d794a98dfb7f69
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-15 08:28:27 +0530

    Time: 467 ms (98.84%), Space: 20.6 MB (68.54%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,33 +1,32 @@
 class Solution:
-    def two_sum(self, nums, target):
-        first, second = 0, len(nums)-1
-        res = []
-        while first < second:
-            curr_sum = nums[first] + nums[second]
-            if curr_sum == target:
-                res.append([nums[first], nums[second]])
-                first += 1
-                second -= 1
-                while first < second and nums[first] == nums[first-1]:
-                    first += 1
-                while first < second and nums[second] == nums[second+1]:
-                    second -= 1
-            elif curr_sum < target:
-                first += 1
-            else:
-                second -= 1
-        return res
-                    
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans = []
         nums.sort()
-        for i, val in enumerate(nums):
+        n = len(nums)
+        ans = []
+
+        for i in range(n):
+            # skip positives
+            if nums[i] > 0:
+                continue
+
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             
-            target = -nums[i]
-            two_sum_result = self.two_sum(nums[i+1:], target)
-            for res in two_sum_result:
-                ans.append([nums[i]] + res)
+            goal = -nums[i]
+            
+            first = i+1
+            second = n-1
+
+            while first < second:
+                curr_sum = nums[first] + nums[second]
+                if curr_sum == goal:
+                    ans.append([nums[i], nums[first], nums[second]])
+                    first += 1
+                    second -= 1
+                    while first < second and nums[first] == nums[first-1]:
+                        first += 1
+                elif curr_sum < goal:
+                    first += 1
+                else:
+                    second -= 1
         return ans
-        
