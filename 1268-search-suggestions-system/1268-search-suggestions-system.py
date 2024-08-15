ed17708f0b28a476d4eda358893b199b3dc5bd1a
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-16 03:09:53 +0530

    Time: 157 ms (45.70%), Space: 24 MB (15.29%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,52 +1,24 @@
-class TrieNode:
-    def __init__(self):
-        self.children = defaultdict(TrieNode)
-        self.end_of_word = False
-        # self.word = None
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-        self.curr_prefix_node = self.root
-    
-    def insert_word(self, word):
-        curr = self.root
-        for ch in word:
-            if ch not in curr.children:
-                curr.children[ch] = TrieNode()
-            curr = curr.children[ch]
-        curr.end_of_word = True
-        # curr.word = word
-
-    def get_words_from_prefix(self, prefix, word):
-        def dfs(curr, word, res):
-            if len(res) == 3:
-                return
-            if curr.end_of_word == True:
-                res.append(word)
-            if not curr.children:
-                return
-            for i in range(26):
-                ch  = chr(ord('a') + i)
-                if ch not in curr.children:
-                    continue
-                dfs(curr.children[ch], word+ch, res)
-        self.curr_prefix_node = self.curr_prefix_node.children[prefix]
-        res = []
-        dfs(self.curr_prefix_node, word, res)
-        
-        return res
-        
-
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        trie_obj = Trie()
-        for product in products:
-            trie_obj.insert_word(product)
-        search_suggestions = []
-        word = ''
-        for ch in searchWord:
-            word += ch
-            search_suggestions.append(trie_obj.get_words_from_prefix(ch, word))
+        class TrieNode:
+            def __init__(self):
+                self.children = collections.defaultdict(TrieNode)
+                self.suggestion = []
+            
+            def add_sugestion(self, product):
+                if len(self.suggestion) < 3:
+                    self.suggestion.append(product)
         
-        return search_suggestions
+        products = sorted(products)
+        root = TrieNode()
+        for p in products:
+            node = root
+            for char in p:
+                node = node.children[char]
+                node.add_sugestion(p)
+        
+        result, node = [], root
+        for char in searchWord:
+            node = node.children[char]
+            result.append(node.suggestion)
+        return result
