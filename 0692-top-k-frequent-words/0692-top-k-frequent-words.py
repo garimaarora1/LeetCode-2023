622c5dc4cf982bdc830586dc29f790ce764c31a8
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-18 01:13:35 +0530

    Time: 97 ms (5.03%), Space: 17.1 MB (7.61%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,56 +1,64 @@
 class TrieNode:
+    
     def __init__(self):
-        self.children = defaultdict(TrieNode)
+        self.children = {}
         self.end_of_word = False
+        self.word = None
 
 class Trie:
+    
     def __init__(self):
         self.root = TrieNode()
-    
-    def add_word(self, word):
+        
+    def insert(self, word):
         curr = self.root
         for ch in word:
             if ch not in curr.children:
                 curr.children[ch] = TrieNode()
             curr = curr.children[ch]
         curr.end_of_word = True
+        curr.word = word
     
-    def get_word(self, curr, prefix, remaining):
-        res = []
-        if remaining[0] == 0:
-            return res
+    def get_top_freq_words(self, remaining):
         
-        if curr.end_of_word == True:
-            remaining[0] -= 1
-            res.append(prefix)
+        def dfs(curr, res, remaining):
             
-        for i in range(26):
-            ch = chr(ord('a') + i)
-            if ch in curr.children:
-                curr_res = self.get_word(curr.children[ch], prefix + ch, remaining)
-                res += curr_res
-        return res
-        
+            if remaining[0] == 0:
+                return
+
+            if curr.word != None:
+                remaining[0] -= 1
+                res.append(curr.word)
+                
+            for i in range(26):
+                ch = chr(ord('a') + i)
+                if ch in curr.children:
+                    dfs(curr.children[ch], res, remaining)
+            return res
+    
+        curr = self.root
+        return dfs(curr, [], remaining)
+
 
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        n = len(words)
-        counter = Counter(words)
         
+        n = len(words)
         buckets = [Trie() for _ in range(n+1)]
         
-        for word, freq in counter.items():
-            buckets[freq].add_word(word)
+        counter = Counter(words)
         
-        remaining = [k]
+        for word, freq in counter.items():
+            buckets[freq].insert(word)
+            
         res = []
+        remaining = [k]
         for i in range(n, 0, -1):
             if remaining[0] == 0:
                 break
-            if buckets[i].root.children:
-                print(1)
-                res.extend(buckets[i].get_word(buckets[i].root, '', remaining))
+            
+            words = buckets[i].get_top_freq_words(remaining)
+            res.extend(words)
         return res
             
-        
         
