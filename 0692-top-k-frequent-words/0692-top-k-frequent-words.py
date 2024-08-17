0fbfee9f274af23b8d058abbf6892253b004aa09
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-17 14:02:09 +0530

    Time: 109 ms (5.03%), Space: 16.9 MB (7.57%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -7,7 +7,7 @@ class Trie:
     def __init__(self):
         self.root = TrieNode()
 
-    def add_word(self, word: str) -> None:
+    def add_word(self, word):
         node = self.root
         for c in word:
             if c not in node.children:
@@ -15,29 +15,32 @@ class Trie:
             node = node.children[c]
         node.end_of_word = True
 
-    def get_words(self, node: TrieNode, prefix: str, k: int) -> List[str]:
-        if k[0] == 0:
+    def get_words(self, node, prefix, k):
+        if k == 0:
             return []
         res = []
         if node.end_of_word:
-            k[0] -= 1
+            k -= 1
             res.append(prefix)
-        for c in sorted(node.children.keys()):  # Sort keys to get words in lexicographical order
-            res += self.get_words(node.children[c], prefix + c, k)
+        for i in range(26):
+            c = chr(ord('a') + i)
+            if c in node.children:
+                res += self.get_words(node.children[c], prefix + c, k)
         return res
 
 class Solution:
+
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         n = len(words)
         cnt = Counter(words)
-        bucket = [Trie() for _ in range(n+1)]
+        bucket = [Trie() for _ in range(n + 1)]
         self.k = k
 
-        def add_word(trie: Trie, word: str) -> None:
+        def add_word(trie, word):
             trie.add_word(word)
 
-        def get_words(trie: Trie, prefix: str) -> List[str]:
-            return trie.get_words(trie.root, prefix, [self.k])
+        def get_words(trie: Trie, prefix):
+            return trie.get_words(trie.root, prefix, self.k)
 
         for word, freq in cnt.items():
             add_word(bucket[freq], word)
@@ -48,4 +51,4 @@ class Solution:
                 return res
             if bucket[i].root.children:
                 res += get_words(bucket[i], '')
-        return res[:self.k]  # Ensure we only return the top k results
+        return res[:self.k]
