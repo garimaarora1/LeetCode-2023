93c5030030a4cd0f90a13691d2ee2833532628dd
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-17 19:07:10 +0530

    Time: 331 ms (20.63%), Space: 22.9 MB (38.06%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,41 +1,51 @@
+from collections import defaultdict
+from typing import List
+
 class TrieNode:
     def __init__(self):
         self.word = None
         self.child = defaultdict(TrieNode)
 
-    def addWord(self, word):
-        curr = self
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str):
+        curr = self.root
         for c in word:
             curr = curr.child[c]
         curr.word = word
 
-    def getWords(self, limit):
+    def getWords(self, node: TrieNode, limit: int) -> List[str]:
         words = []
 
-        def dfs(curr):
-            if len(words) == limit: return
-            if curr.word != None:
+        def dfs(curr: TrieNode):
+            if len(words) == limit:
+                return
+            if curr.word is not None:
                 words.append(curr.word)
             for c in sorted(curr.child.keys()):
-                if c in curr.child:
-                    dfs(curr.child[c])
+                dfs(curr.child[c])
 
-        dfs(self)
+        dfs(node)
         return words
+
+    def searchPrefix(self, prefix: str) -> List[List[str]]:
+        curr = self.root
+        result = []
+        for c in prefix:
+            if curr is not None and c in curr.child:
+                curr = curr.child[c]
+                result.append(self.getWords(curr, 3))
+            else:
+                curr = None
+                result.append([])
+        return result
 
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        root = TrieNode()
+        trie = Trie()
         for product in products:
-            root.addWord(product)
-
-        ans = []
-        curr = root
-        for c in searchWord:
-            if curr != None and c in curr.child:
-                curr = curr.child[c]
-                ans.append(curr.getWords(3))
-            else:
-                curr = None
-                ans.append([])
-        return ans
+            trie.addWord(product)
+        
+        return trie.searchPrefix(searchWord)
