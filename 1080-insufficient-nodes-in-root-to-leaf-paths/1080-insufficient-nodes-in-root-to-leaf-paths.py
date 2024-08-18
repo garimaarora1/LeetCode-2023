a71f895d5db8552210679aab88c29833108258e3
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-18 23:51:47 +0530

    Time: 62 ms (74.72%), Space: 17.8 MB (28.93%) - LeetHub

---
+++ 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sufficientSubset(self, root: TreeNode, limit: int) -> TreeNode:
+        def dfs(node, path_sum):
+            if not node:
+                return False
+            path_sum += node.val
+            if not node.left and not node.right:
+                return path_sum >= limit
+            left = dfs(node.left, path_sum)
+            right = dfs(node.right, path_sum)
+            if not left:
+                node.left = None
+            if not right:
+                node.right = None
+            return left or right
+        result = dfs(root, 0)
+        return root if result else None
