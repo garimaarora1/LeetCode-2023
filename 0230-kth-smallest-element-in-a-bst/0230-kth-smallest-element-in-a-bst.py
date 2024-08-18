64da539da086f1450e3c638cdf0b2c590135afc1
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-18 15:27:28 +0530

    Time: 45 ms (57.74%), Space: 19.4 MB (65.74%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -1,18 +1,17 @@
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         
-        def inorder_traversal(root):
+        def inorder_traversal(root, remaining):
             if not root:
                 return None
 
-            left_result = inorder_traversal(root.left)
+            left_result = inorder_traversal(root.left, remaining)
             if left_result is not None:
                 return left_result
-
-            self.k -= 1
-            if self.k == 0:
+            remaining[0] -= 1
+            if remaining[0] == 0:
                 return root.val
 
-            return inorder_traversal(root.right)
-        self.k = k
-        return inorder_traversal(root)
+            return inorder_traversal(root.right, remaining)
+        remaining = [k]
+        return inorder_traversal(root, remaining)
