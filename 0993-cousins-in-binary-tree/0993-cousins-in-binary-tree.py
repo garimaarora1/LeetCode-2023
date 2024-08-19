43106ec3c94d15396abe283adce8aa885135788d
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-19 23:23:54 +0530

    Time: 40 ms (28.63%), Space: 16.5 MB (52.88%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.py
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.py
@@ -6,35 +6,38 @@
 #         self.right = right
 class Solution:
     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
-    
         queue = deque([root])
         
-        
         while queue:
-            sibling, cousin = False, False
             n = len(queue)
-            
+                            
+            sibling = False
+            cousin = False
+
             for _ in range(n):
                 
                 node = queue.popleft()
-                if node is None:
+                
+                if not node:
                     sibling = False
+                
                 else:
                     if node.val == x or node.val == y:
-                        
                         if not cousin:
-                            sibling, cousin = True, True
-                        
+                            cousin, sibling = True, True
                         else:
                             return not sibling
+                    
                     if node.left:
                         queue.append(node.left)
-
+                    
                     if node.right:
                         queue.append(node.right)
-                
+                    
                     queue.append(None)
             if cousin:
                 return False
         return False
         
+                    
+                    
