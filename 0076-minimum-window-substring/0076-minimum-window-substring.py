c9fe5604c4927b6561bb3c1f5907b7d41f96e93f
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-21 04:39:27 +0530

    Time: 129 ms (50.66%), Space: 17.3 MB (22.06%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,24 +1,29 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        counter = Counter(t)
-        unique_chars = len(counter)
-        i = j = 0
+        counter_t = Counter(t)
+        unique_ch = len(counter_t)
+        i, j = 0, 0
         mini = float('inf')
         res = []
         while j < len(s):
-            ch = s[j]
-            if ch in counter:
-                counter[ch] -= 1
-                if counter[ch] == 0:
-                    unique_chars -= 1
-            while unique_chars == 0 and i <= j:
+            if s[j] in counter_t:
+                counter_t[s[j]] -= 1
+                if counter_t[s[j]] == 0:
+                    unique_ch -= 1
+            
+            while unique_ch == 0 and i <= j:
                 if j-i+1 < mini:
                     mini = j-i+1
-                    res = (i,j)
-                if s[i] in counter:
-                    counter[s[i]] += 1
-                    if counter[s[i]] == 1:
-                        unique_chars += 1
+                    res = [i, j]
+                
+                if s[i] in counter_t:
+                    counter_t[s[i]] += 1
+                    if counter_t[s[i]] == 1:
+                        unique_ch += 1
                 i += 1
             j += 1
-        return "" if mini== float('inf') else s[res[0]:res[1]+1]
+        return "" if mini == float('inf') else s[res[0]:res[1]+1]
+                
+        
+        
+        
