61b0f235d8b8207f574616048d8ed89189c40da3
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-21 15:12:51 +0530

    Time: 70 ms (48.45%), Space: 16.7 MB (18.25%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,28 +1,25 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        counter = defaultdict(int)
-        for ch in s1:
-            counter[ch] += 1
-        unique_ch = len(counter)
         i, j = 0, 0
+        counter = Counter(s1)
+        unique_characters = len(counter)
+        
         while j < len(s2):
-            ch = s2[j]
-            if ch in counter:
-                counter[ch] -= 1
-                if counter[ch] == 0:
-                    unique_ch -= 1
-                
-                if unique_ch == 0:
+            if s2[j] in counter:
+                counter[s2[j]] -= 1
+                if counter[s2[j]] == 0:
+                    unique_characters -= 1
+                if unique_characters == 0:
                     return True
-            
             if j-i+1 == len(s1):
                 if s2[i] in counter:
                     counter[s2[i]] += 1
                     if counter[s2[i]] == 1:
-                        unique_ch += 1
-                    
+                        unique_characters += 1
                 i += 1
             j += 1
         return False
+            
                 
+        
         
