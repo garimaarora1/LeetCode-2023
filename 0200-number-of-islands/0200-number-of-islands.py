dd6721adfdc7e71d334a05277ef8caeb57025138
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-24 01:55:59 +0530

    Time: 270 ms (30.86%), Space: 22.2 MB (43.18%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,38 +1,49 @@
+class UnionFind:
+    def __init__(self, row, col):
+        self.parents = [i for i in range(row*col)]
+
+    def find(self, x):
+        if self.parents[x] == x:
+            return x
+        return self.find(self.parents[x])
+
+    def union(self, x, y):
+        parent_x = self.find(x)
+        parent_y = self.find(y)
+        
+        if parent_x != parent_y:
+            if parent_x < parent_y:
+                self.parents[parent_x] = parent_y
+            else:
+                self.parents[parent_y] = parent_x
+            
+            return True
+        return False
+
 class Solution:
+    
     def numIslands(self, grid: List[List[str]]) -> int:
-        ones_count = 0 
         row = len(grid)
         col = len(grid[0])
-
-        parent = [i for i in range(row*col)]
-        
-        def find(x):
-            if parent[x] != x:
-                parent[x] = find(parent[x])
-            return parent[x]
-
-
-        def union(x, y):
-            x_parent, y_parent = find(x), find(y)
-            if x_parent == y_parent:
-                return False
-            if x_parent < y_parent:
-                parent[y_parent] = x_parent
-            else:
-                parent[x_parent] = y_parent
-            return True
-        
-        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+        directions = [[0, -1], [1, 0], [0, 1], [-1, 0]]
+        count = 0
+        uf = UnionFind(row, col)
         for i in range(row):
             for j in range(col):
                 if grid[i][j] == '1':
-                    ones_count += 1
-                    position_1 = i*col + j 
+                    count += 1
+                    pos1 = i * col + j
+                    
                     for dx, dy in directions:
-                        x = i + dx
-                        y = j + dy
-                        if 0<=x<row and 0<=y<col and grid[x][y] == '1':
-                            position_2 = x*col + y
-                            if union(position_2, position_1):
-                                ones_count -= 1
-        return ones_count
+                        dr = i + dx
+                        dc = j + dy
+                        if 0<=dr<row and 0<=dc<col and grid[dr][dc] == '1':
+                            pos2 = dr * col + dc
+                            if uf.union(pos1, pos2):
+                                count -= 1
+        return count
+                                
+                        
+                        
+                        
+                        
