dcc83c0215173e22a01fd314088045189bf2aa25
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-24 17:04:04 +0530

    Time: 83 ms (90.51%), Space: 18.3 MB (60.94%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,27 +1,29 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
-        indegree = [0] * numCourses
+        in_degrees = [0] * numCourses
+        queue = deque()
+        count = 0
+        res = []
+
         for u, v in prerequisites:
             graph[v].append(u)
-            indegree[u] += 1
-            
-        queue = deque()
-        for i in range(numCourses):
-            if indegree[i] == 0:
+            in_degrees[u] += 1
+        
+        for i, val in enumerate(in_degrees):
+            if val == 0:
                 queue.append(i)
-        
-        res = []
-        
+
         while queue:
             node = queue.popleft()
             res.append(node)
+            count += 1
+    
             for adj in graph[node]:
-                indegree[adj] -= 1
-                if indegree[adj] == 0:
+                in_degrees[adj] -= 1
+                if in_degrees[adj] == 0:
                     queue.append(adj)
         
-
-        return res if len(res) == numCourses else []
-            
+        return res if numCourses == count else []
+        
         
