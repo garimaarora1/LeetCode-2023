1893d3b6343d608006325a3575f0c83b6eec1f1f
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-25 22:22:16 +0530

    Time: 1530 ms (24.69%), Space: 77.7 MB (60.77%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
@@ -1,20 +1,25 @@
 class Solution:
     def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
         graph = defaultdict(list)
-        for u,v in roads:
+        
+        for u, v in roads:
             graph[u].append(v)
             graph[v].append(u)
-        
-        def dfs(node, k, parent):
-            total_count = 0
-            total_fuel = 0
+    
+        def dfs(node, parent):
+            nonlocal min_total_fuel
+            
+            passengers = 0
+            
             for adj in graph[node]:
                 if adj != parent:
-                    node_count, fuel = dfs(adj, k, node)
-                    total_count += node_count
-                    total_fuel += fuel + ceil((node_count)/k)  
-            return total_count+1, total_fuel
+                    p = dfs(adj, node)
+                    passengers += p
+                    min_total_fuel += ceil(p/seats)
+            return passengers + 1
         
-        _, fuel = dfs(0, seats, -1)
-        return fuel
+        min_total_fuel = 0
+        dfs(0, -1)
+        return min_total_fuel
+        
         
