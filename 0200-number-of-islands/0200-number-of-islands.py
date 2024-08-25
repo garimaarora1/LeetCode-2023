cfc226022b85504cc7446126bc06ea68dc79e7d3
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-26 01:27:30 +0530

    Time: 242 ms (60.95%), Space: 24.2 MB (35.27%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,51 +1,30 @@
-class UnionFind:
-    def __init__(self, row, col):
-        self.parents = [i for i in range(row*col)]
-
-    def find(self, x):
-        if self.parents[x] == x:
-            return x
-        self.parents[x] = self.find(self.parents[x])
-        return self.parents[x]
-
-    def union(self, x, y):
-        parent_x = self.find(x)
-        parent_y = self.find(y)
-        
-        if parent_x != parent_y:
-            if parent_x < parent_y:
-                self.parents[parent_y] = parent_x
-            else:
-                self.parents[parent_x] = parent_y
-            
-            return True
-        return False
-
 class Solution:
-    
     def numIslands(self, grid: List[List[str]]) -> int:
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        
+        def bfs(i, j):
+            queue = deque()
+            queue.append((i, j))
+            
+            while queue:
+                i, j = queue.popleft()
+                for dr, dc in directions:
+                    dx = i + dr
+                    dy = j + dc
+                    if 0<=dx<row and 0<=dy<col and grid[dx][dy] == '1' and (dx, dy) not in visited:
+                        visited.add((dx, dy))
+                        queue.append((dx, dy))
+            
         row = len(grid)
         col = len(grid[0])
-        directions = [[0, -1], [1, 0], [0, 1], [-1, 0]]
-        count = 0
-        uf = UnionFind(row, col)
+        visited = set()
+        components = 0
+
         for i in range(row):
             for j in range(col):
-                if grid[i][j] == '1':
-                    count += 1
-                    pos1 = i * col + j
+                if grid[i][j] == '1' and (i, j) not in visited:
+                    visited.add((i, j))
+                    bfs(i, j)
+                    components += 1
                     
-                    for dx, dy in directions:
-                        dr = i + dx
-                        dc = j + dy
-                        if 0<=dr<row and 0<=dc<col and grid[dr][dc] == '1':
-                            pos2 = dr * col + dc
-                            if uf.union(pos1, pos2):
-                                print(1)
-                                count -= 1
-        return count
-                                
-                        
-                        
-                        
-                        
+        return components
