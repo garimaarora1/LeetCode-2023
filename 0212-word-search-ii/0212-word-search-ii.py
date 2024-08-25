2a4ba18d257a4a071ccb745d8ce5f96e18c94739
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-26 03:42:07 +0530

    Time: 1428 ms (72.94%), Space: 19.9 MB (12.01%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,16 +1,16 @@
 class TrieNode:
-
+    
     def __init__(self):
-        self.children = defaultdict(TrieNode)
+        self.children = {}
         self.end_of_word = False
         self.ref = 0
 
 class Trie:
-
+    
     def __init__(self):
         self.root = TrieNode()
-
-    def add_word(self, word):
+    
+    def insert(self, word):
         curr = self.root
         curr.ref += 1
         for ch in word:
@@ -20,51 +20,51 @@ class Trie:
             curr.ref += 1
         curr.end_of_word = True
     
-    def remove_word(self, word):
+    def remove(self, word):
         curr = self.root
         curr.ref -= 1
         for ch in word:
             curr = curr.children[ch]
             curr.ref -= 1
         curr.end_of_word = False
-
+            
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        words_found = []
         
         directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-        def dfs(i, j, curr, word):
-            if curr.end_of_word == True:
-                res.append(word)
-                trie_obj.remove_word(word)
+        
+        def dfs(i, j, curr, prefix):
             
-            for dx, dy in directions:
-                x = i + dx
-                y = j + dy
+            if curr.end_of_word:
+                trie.remove(prefix)
+                words_found.append(prefix)
+            
+            for dr, dc in directions:
+                dx = i + dr
+                dy = j + dc
                 
-                if 0<=x<row and 0<=y<col and (x, y) not in visited and board[x][y] in curr.children and curr.children[board[x][y]].ref > 0:
-                    visited.add((x, y))
-                    dfs(x, y, curr.children[board[x][y]], word + board[x][y])
-                    visited.remove((x, y))
+                if 0<=dx<row and 0<=dy<col and board[dx][dy] in curr.children and (dx, dy) not in visited and curr.ref > 0:
+                    visited.add((dx, dy))
+                    dfs(dx, dy, curr.children[board[dx][dy]], prefix+board[dx][dy])
+                    visited.remove((dx, dy))
+                
             
-            
-        trie_obj = Trie()
-        curr = trie_obj.root
+        trie = Trie()
+        curr = trie.root
         
         for word in words:
-            trie_obj.add_word(word)
-            
+            trie.insert(word)
+
         row = len(board)
         col = len(board[0])
         visited = set()
-        res = []
-
+        
         for i in range(row):
             for j in range(col):
                 if board[i][j] in curr.children and curr.ref > 0:
                     visited.add((i, j))
                     dfs(i, j, curr.children[board[i][j]], board[i][j])
                     visited.remove((i, j))
-                    
-        return res
-            
-            
+        return words_found
+        
