84fe3f53a7ad4092e66f7bbf2e9a3489253705a2
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-25 19:11:44 +0530

    Time: 1059 ms (94.44%), Space: 55.1 MB (68.13%) - LeetHub

--- 2497-maximum-star-sum-of-a-graph/2497-maximum-star-sum-of-a-graph.py
+++ 2497-maximum-star-sum-of-a-graph/2497-maximum-star-sum-of-a-graph.py
@@ -1,27 +1,26 @@
 class Solution:
     def maxStarSum(self, vals: List[int], edges: List[List[int]], K: int) -> int:
-        if edges == []:
+        if not edges:
             return max(vals)
+        
         graph = defaultdict(list)
         for u, v in edges:
             graph[u].append(v)
             graph[v].append(u)
-        max_sum = max(vals)
-
-        for key, nodes in graph.items():
-            # print(key, nodes)
-            curr_sum = vals[key]
-            values = []
-            star_edge_values = []
-            for node in nodes:
-                values.append(vals[node])
-            values.sort(reverse=True)
-            for value in values[:K]:
-                if value < 0:
-                    break
-                star_edge_values.append(value)
-            curr_sum += sum(star_edge_values)
-            max_sum = max(max_sum, curr_sum)
-        return max_sum
-            
         
+        max_sum = max(vals)
+        
+        for key, nodes in graph.items():
+            curr_sum = vals[key]
+            min_heap = []
+            for node in nodes:
+                neighbor_value = vals[node]
+                if neighbor_value > 0:
+                    heapq.heappush(min_heap, neighbor_value)
+                    if len(min_heap) > K:
+                        heapq.heappop(min_heap)
+            
+            curr_sum += sum(min_heap)
+            max_sum = max(max_sum, curr_sum)
+        
+        return max_sum
