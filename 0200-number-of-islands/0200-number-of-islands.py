463416a08e35ac65bbd4927f2088affe966fe657
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-26 01:54:36 +0530

    Time: 291 ms (21.58%), Space: 22.9 MB (42.68%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,30 +1,51 @@
+class UnionFind:
+    
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [0] * n
+    
+    def find(self, x):
+        if self.parent[x] == x:
+            return x
+        self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        x_parent = self.find(x)
+        y_parent = self.find(y)
+        
+        if x_parent != y_parent:
+            if self.rank[x_parent] < self.rank[y_parent]:
+                self.parent[x_parent] = y_parent
+            elif self.rank[x_parent] > self.rank[y_parent]:
+                self.parent[y_parent] = x_parent
+            else:
+                self.parent[y_parent] = x_parent
+                self.rank[x_parent] += 1
+            return True
+        return False
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         
-        def bfs(i, j):
-            queue = deque()
-            queue.append((i, j))
-            
-            while queue:
-                i, j = queue.popleft()
-                for dr, dc in directions:
-                    dx = i + dr
-                    dy = j + dc
-                    if 0<=dx<row and 0<=dy<col and grid[dx][dy] == '1' and (dx, dy) not in visited:
-                        visited.add((dx, dy))
-                        queue.append((dx, dy))
-            
         row = len(grid)
         col = len(grid[0])
-        visited = set()
         components = 0
-
+        n = row * col
+        uf = UnionFind(n)
+        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
         for i in range(row):
             for j in range(col):
-                if grid[i][j] == '1' and (i, j) not in visited:
-                    visited.add((i, j))
-                    bfs(i, j)
+                if grid[i][j] == '1': 
                     components += 1
+                    pos1 = i * col + j
                     
+                    for dr, dc in directions:
+                        dx = i + dr
+                        dy = j + dc
+                        
+                        pos2 = dx * col + dy
+                        
+                        if 0<=dx<row and 0<=dy<col and grid[dx][dy] == '1' and uf.union(pos1, pos2):
+                            components -= 1
         return components
