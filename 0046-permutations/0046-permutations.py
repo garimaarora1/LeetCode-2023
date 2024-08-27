42f5cd5159c7a11bbed307c1580fb2bfb4838c79
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-27 16:55:41 +0530

    Time: 40 ms (58.66%), Space: 16.9 MB (6.27%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -4,16 +4,28 @@ class Solution:
         curr_ans = []
         n = len(nums)
         def dfs(i, curr_ans):
-            if len(curr_ans) == n:
-                ans.append(curr_ans.copy())
+            if i == n:
+                ans.append(nums.copy())
                 return
 
             for j in range(i, n):
-                curr_ans.append(nums[j])
+                # curr_ans.append(nums[j])
+                # important
                 nums[i], nums[j] = nums[j], nums[i]
                 dfs(i+1, curr_ans)
-                curr_ans.pop()
+                # curr_ans.pop()
+                # important
                 nums[i], nums[j] = nums[j], nums[i]
         
         dfs(0, curr_ans)
         return ans
+    
+# res = [[1, 2, 3], ]         BT
+
+# nums = [1, 2, 3]
+#     | dfs(3, [1, 2, 3]) 3, [1, 2, 3]    | 
+#     | dfs(2, [1, 2]) 2, [1, 2, 3]       | [1, 2]
+#     | dfs(1, [1]) j: 1, [1, 2]          | [1]  i:1  j: 2 [1, 3] nums = [1, 3, 2]
+#     | dfs(0, []): j: 0, [1]             |
+    
+    
