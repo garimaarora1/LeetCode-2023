5713ad3568524d7233710577e362966bafa5f38d
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-27 16:57:29 +0530

    Time: 42 ms (45.98%), Space: 16.8 MB (31.31%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -9,23 +9,9 @@ class Solution:
                 return
 
             for j in range(i, n):
-                # curr_ans.append(nums[j])
-                # important
                 nums[i], nums[j] = nums[j], nums[i]
                 dfs(i+1, curr_ans)
-                # curr_ans.pop()
-                # important
                 nums[i], nums[j] = nums[j], nums[i]
         
         dfs(0, curr_ans)
         return ans
-    
-# res = [[1, 2, 3], ]         BT
-
-# nums = [1, 2, 3]
-#     | dfs(3, [1, 2, 3]) 3, [1, 2, 3]    | 
-#     | dfs(2, [1, 2]) 2, [1, 2, 3]       | [1, 2]
-#     | dfs(1, [1]) j: 1, [1, 2]          | [1]  i:1  j: 2 [1, 3] nums = [1, 3, 2]
-#     | dfs(0, []): j: 0, [1]             |
-    
-    
