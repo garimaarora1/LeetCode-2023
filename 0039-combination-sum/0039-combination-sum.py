07f1a613411d2dd96df679c7211031d0113893ec
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-27 15:23:08 +0530

    Time: 55 ms (45.77%), Space: 16.7 MB (11.03%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,13 +1,21 @@
 class Solution:
+    """
+    Find all unique combinations in `candidates` where the candidate numbers sum up       to `target`.
+    Each number in `candidates` can be used an unlimited number of times.
+    Time Complexity: O(2^N) where N is the length of `candidates`
+    Space Complexity: O(N) where N is the length of the current combination
+    """
+
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
         curr_ans = []
         n = len(candidates)
+        
         def dfs(i, curr_ans, curr_sum):
+            if curr_sum > target:
+                return
             if curr_sum == target:
                 ans.append(curr_ans.copy())
-                return
-            if curr_sum > target:
                 return
             
             for j in range(i, n):
@@ -17,6 +25,4 @@ class Solution:
                 curr_sum -= curr_ans.pop()
         
         dfs(0, curr_ans, 0)
-        
         return ans
-        
