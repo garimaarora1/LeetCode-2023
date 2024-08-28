60121f2b0559fbeab21808de66049c8d6e78442b
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-28 21:31:06 +0530

    Time: 47 ms (76.69%), Space: 16.5 MB (86.14%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,32 +1,35 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        """
+        calculate fresh oranges, rotten oranges, rotten organges go in queue, return minutes if fresh oranges == 0 else -1
+        also only do bps uptil you have fresh organges and queu
+        """
         row = len(grid)
         col = len(grid[0])
-        
         queue = deque()
         remaining_oranges = 0
+        minutes = 0
+
         for i in range(row):
             for j in range(col):
                 if grid[i][j] == 2:
-                    queue.append((i,j))
+                    queue.append((i, j))
                 elif grid[i][j] == 1:
                     remaining_oranges += 1
         
-        minutes = 0
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         while queue and remaining_oranges:
-            directions = [[0,1], [1,0], [0,-1], [-1,0]]
-            minutes += 1
-            for i in range(len(queue)):
-                x, y = queue.popleft()
-
-                for dr, dc in directions:
-                    dx = x + dr
-                    dy = y + dc
-
-                    if 0<=dx< row and 0<=dy<col and grid[dx][dy] == 1:
-                        grid[dx][dy] = 2
-                        queue.append((dx, dy))
+            n = len(queue)
+            for _ in range(n):
+                i, j = queue.popleft()
+                for dx, dy in directions:
+                    x = i + dx
+                    y = j + dy
+                    
+                    if 0<=x<row and 0<=y<col and grid[x][y] == 1:
+                        grid[x][y] = 2
+                        queue.append((x, y))
                         remaining_oranges -= 1
-
+            minutes += 1
         return minutes if remaining_oranges == 0 else -1
         
