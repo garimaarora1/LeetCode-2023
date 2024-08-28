d31d18a270fea65c5d3ea65015fab9961a1d80da
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-28 13:47:17 +0530

    Time: 44 ms (31.94%), Space: 16.5 MB (86.18%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -4,28 +4,32 @@ class Solution:
         n = len(s)
         if n < 3 or n > 12:
             return []
-
-        def dfs(i, parts):
+        
+        def dfs(curr_idx, parts):
+            
             if len(parts) > 4:
                 return
             
-            if i == n and len(parts) == 4:
+            if curr_idx == n and len(parts) == 4:
                 ans.append('.'.join(parts))
                 return
             
-            # Try each possible part length (1 to 3 characters)
-            for j in range(i, min(i+3, n)):
-                part = s[i: j+1]
+            for j in range(curr_idx, min(curr_idx+3, n)):
                 
-                # Skip invalid parts
+                part = s[curr_idx: j+1]
+                
+                # skip invalid parts
                 if len(part) > 1 and part[0] == '0':
-                    continue
-                if len(part) >= 4 or int(part) > 255:
-                    continue
-                # Add part and recurse
+                    return
+                
+                if int(part) > 255:
+                    return
+                
                 parts.append(part)
                 dfs(j+1, parts)
                 parts.pop()
+                
         
         dfs(0, [])
+        
         return ans
