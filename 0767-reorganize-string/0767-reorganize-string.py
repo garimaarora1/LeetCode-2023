8f17674dd248de9c63f965a1fe44ab731049b439
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-28 17:07:31 +0530

    Time: 38 ms (51.18%), Space: 16.5 MB (93.22%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,29 +1,29 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        counter = Counter(s)
-        # max_heap
-        heap = []
-        
-        for key, value in counter.items():
-            heapq.heappush(heap, (-value, key))
-        heapq.heapify(heap)
-        
-        prev = None
-        res = ''
-        
-        while heap or prev:
-            if prev and not heap:
-                return ""
-            
-            freq, ch = heapq.heappop(heap)
-            res += ch
-            freq += 1
-            
-            if prev:
-                heapq.heappush(heap, prev)
-                prev = None
-            if not prev:
-                if freq != 0:
-                    prev = (freq, ch)
-        
-        return res
+        char_counts = Counter(s)
+        max_count, letter = 0, ''
+        for char, count in char_counts.items():
+            if count > max_count:
+                max_count = count
+                letter = char
+        if max_count > (len(s) + 1) // 2:
+            return ""
+        ans = [''] * len(s)
+        index = 0
+
+        # Place the most frequent letter
+        while char_counts[letter] != 0:
+            ans[index] = letter
+            index += 2
+            char_counts[letter] -= 1
+
+        # Place rest of the letters in any order
+        for char, count in char_counts.items():
+            while count > 0:
+                if index >= len(s):
+                    index = 1
+                ans[index] = char
+                index += 2
+                count -= 1
+
+        return ''.join(ans)
