e27dd1f77c097e4da131b2e45a7aaf6da3e64c38
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-29 00:35:48 +0530

    Time: 73 ms (79.82%), Space: 19.4 MB (75.03%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,26 +3,35 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeKLists(self, lists):
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        if not l1 or not l2:
+            return l1 or l2
+        l3 = ListNode()
+        head = l3
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                l3.next = l1
+                l1 = l1.next
+            else:
+                l3.next = l2
+                l2 = l2.next
+            l3 = l3.next
+        if l1:
+            l3.next = l1
+        if l2:
+            l3.next = l2
+        return head.next
+
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists:
             return None
         if len(lists) == 1:
             return lists[0]
+
         mid = len(lists) // 2
         
-        l, r = self.mergeKLists(lists[:mid]), self.mergeKLists(lists[mid:])
-        return self.merge(l, r)
-    
-    def merge(self, l, r):
-        dummy = p = ListNode()
-        while l and r:
-            if l.val < r.val:
-                p.next = l
-                l = l.next
-            else:
-                p.next = r
-                r = r.next
-            p = p.next
-        p.next = l or r
-        return dummy.next
+        list1, list2 = self.mergeKLists(lists[:mid]), self.mergeKLists(lists[mid:])
+        
+        return self.mergeTwoLists(list1, list2)
