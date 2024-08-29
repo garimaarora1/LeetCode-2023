5fc22606eb1b76e112b629c4507e29aed2963154
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-30 01:07:05 +0530

    Time: 694 ms (56.04%), Space: 20.6 MB (60.31%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
@@ -1,12 +1,21 @@
 class Solution:
-    def minSwaps(self, a: List[int]) -> int:
-        no_of_ones=a.count(1)
-        a=a+a
-        ones_in_window,max_ones_in_window=0,0
-        for i in range(len(a)):
-            if a[i]:
-                ones_in_window+=1
-            if i>=no_of_ones and a[i-no_of_ones]:
-                ones_in_window-=1
-            max_ones_in_window=max(ones_in_window,max_ones_in_window)
-        return no_of_ones - max_ones_in_window
+    def minSwaps(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        totalOnes = sum(nums)
+
+        if totalOnes == 0:
+            return 0
+
+        # Initialize onesCount in the first window
+        onesCount = sum(nums[:totalOnes])
+        minSwaps = totalOnes - onesCount
+
+        # Sliding window approach
+        for start in range(1, n):
+            end = (start + totalOnes - 1) % n
+            onesCount -= nums[start - 1]
+            onesCount += nums[end]
+            minSwaps = min(minSwaps, totalOnes - onesCount)
+
+        return minSwaps
