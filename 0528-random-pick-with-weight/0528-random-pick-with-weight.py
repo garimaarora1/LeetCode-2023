789bd6dfdd6d3acc910e9a26d1aa9713eafc0c19
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-30 23:59:21 +0530

    Time: 184 ms (29.69%), Space: 21.1 MB (49.95%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -6,17 +6,28 @@ class Solution:
     def __init__(self, w: List[int]):
         self.prefix_sum = []
         self.total_sum = 0
+        self.total_weights = len(w)
+
         for weight in w:
             self.total_sum += weight
             self.prefix_sum.append(self.total_sum)
 
     def pickIndex(self) -> int:
+        low, high = 0, self.total_weights - 1
         target = random.randint(1, self.total_sum)
-        for i, curr_prefix_sum in enumerate(self.prefix_sum):
-            if target <= curr_prefix_sum:
-                return i
-                
 
+        while low <= high:
+            mid = (low + high) // 2
+            
+            if self.prefix_sum[mid] == target:
+                return mid
+            if self.prefix_sum[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+            """If the loop finishes without directly finding the target, low will point to the first index where the cumulative sum is greater than the target, which is the correct index to return."""
+
+        return low
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
