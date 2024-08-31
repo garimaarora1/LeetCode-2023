a3fb5c93fd5f712813835e5927ab30ff984b8975
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-31 20:39:25 +0530

    Time: 131 ms (89.99%), Space: 26.6 MB (56.06%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def singleNonDuplicate(self, nums):
+        low = 0
+        high = len(nums)-1
+
+        while low <= high:
+            mid = (low + high) // 2
+            
+            if( mid-1 < 0 or nums[mid-1] != nums[mid]) and \
+                (mid+1 == len(nums) or nums[mid+1] != nums[mid]):
+                return nums[mid]
+            leftsize = mid-1 if nums[mid-1] == nums[mid] else mid
+
+            if leftsize % 2 == 0:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return -1
+
+        
