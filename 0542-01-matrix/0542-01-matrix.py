2cc2fb180c966fc91c188abc0b00e07408dd1f09
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-31 20:23:11 +0530

    Time: 467 ms (49.05%), Space: 19.7 MB (47.62%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -4,22 +4,26 @@ class Solution:
         col = len(mat[0])
         queue = deque()
         visited = set()
+        ones = 0
         for i in range(row):
             for j in range(col):
                 if mat[i][j] == 0:
                     visited.add((i, j))
                     queue.append((i, j))
+                else:
+                    ones += 1
                     
         directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
         curr_dist = 1
 
-        while queue:
+        while queue and ones:
             for _ in range(len(queue)):
                 i, j = queue.popleft()
                 for dx, dy in directions:
                     x = i + dx
                     y = j + dy
                     if 0<=x<row and 0<=y<col and (x, y) not in visited:
+                        ones -= 1
                         mat[x][y] = curr_dist
                         visited.add((x, y))
                         queue.append((x, y))
