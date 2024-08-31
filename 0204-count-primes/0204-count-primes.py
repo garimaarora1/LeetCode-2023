5c5a1a92acaac7ee7ed52c2d7ddbe5e26110d564
Garima Arora <garimaarora.ga2@gmail.com>
2024-08-31 23:44:06 +0530

    Time: 1222 ms (81.55%), Space: 94.4 MB (10.95%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n <= 2:
+            return 0
+        numbers = [False, False] + [True] * (n - 2)
+
+        for p in range(2, int(sqrt(n)) + 1):
+            if numbers[p]:
+                # Set all multiples of p to false because they are not prime.
+                for multiple in range(p * p, n, p):
+                    numbers[multiple] = False
+
+        return sum(numbers)
