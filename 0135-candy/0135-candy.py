20b538138af24d51230d9f17f3e2b3447513b04a
Garima Arora <garimaarora.ga2@gmail.com>
2024-09-02 01:17:47 +0530

    Time: 123 ms (63.25%), Space: 20.1 MB (7.37%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -9,12 +9,11 @@ class Solution:
                 continue
             
             peak=0
-            while ratings[i]>ratings[i-1]:
+            while i<n and ratings[i]>ratings[i-1]:
                 peak+=1
                 candies+=peak
                 i+=1
-                if i==n:
-                    return candies
+
             slope=0
             while i<n and ratings[i]<ratings[i-1]:
                 slope+=1
