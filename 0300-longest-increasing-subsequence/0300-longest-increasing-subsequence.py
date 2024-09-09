ce6c0d24d8723142b00e09e4f8a67588b08d3184
Garima Arora <garimaarora.ga2@gmail.com>
2024-09-09 20:39:11 +0530

    Time: 65 ms (89.90%), Space: 16.9 MB (69.34%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,29 +1,24 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        def bisect_left(dp, target):
-            left, right = 0, len(dp)
-
-            # Binary search
-            while left < right:
+        # finds the leftmost position in the dp where target can be inserted while keeping dp sorted.
+        def binary_search(left, right, target):
+            while left <= right:
                 mid = (left + right) // 2
                 if dp[mid] < target:
                     left = mid + 1
                 else:
-                    right = mid
-
+                    right = mid - 1
             return left
+
         dp = []
 
         for num in nums:
-            # Use custom bisect_left to find the correct position
-            i = bisect_left(dp, num)
+            i = binary_search(0, len(dp) - 1, num)
 
-            # If num is larger than any element in dp, append it
             if i == len(dp):
                 dp.append(num)
             else:
-                # Otherwise, replace the element at the found index
                 dp[i] = num
-
-        # The length of dp is the length of the longest increasing subsequence
+        # note: dp does not contian the Longest Increasing Subsequence
+        # dp[1] is the smallest end element of an increasing subsequence of length 2, and so on.
         return len(dp)
