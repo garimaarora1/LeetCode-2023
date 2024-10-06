ce85ef0a5151796a258ce5340e61904c1eb492ea
Garima Arora <garimaarora.ga2@gmail.com>
2024-10-06 19:56:49 +0530

    Time: 100 ms (36.82%), Space: 18.4 MB (61.16%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,14 +1,9 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        """
-        courses: 0 n-1
-        1 -> 0
-        """
         graph = defaultdict(list)
         in_degrees = [0] * numCourses
         queue = deque()
-        count = 0
-
+        course_count = 0
         for u, v in prerequisites:
             graph[v].append(u)
             in_degrees[u] += 1
@@ -16,15 +11,12 @@ class Solution:
         for i, val in enumerate(in_degrees):
             if val == 0:
                 queue.append(i)
-
+        
         while queue:
             node = queue.popleft()
-            count += 1
+            course_count += 1
             for adj in graph[node]:
                 in_degrees[adj] -= 1
                 if in_degrees[adj] == 0:
                     queue.append(adj)
-        
-        return count == numCourses
-        
-        
+        return course_count == numCourses
