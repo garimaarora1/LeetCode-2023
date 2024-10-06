f3da2fafd6ef54770f8a3474f692162d362fb086
Garima Arora <garimaarora.ga2@gmail.com>
2024-10-07 00:30:37 +0530

    Time: 3710 ms (72.57%), Space: 16.6 MB (73.23%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,32 +1,37 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         
-        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
         n = len(word)
+        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        
         def dfs(i, j, curr_idx):
             if curr_idx == n:
                 return True
-    
-            for dx, dy in directions:
-                x = i + dx
-                y = j + dy
-                
-                if 0<=x< row and 0<=y<col and (x, y) not in visited and board[x][y] == word[curr_idx]:
-                    visited.add((x, y))
-                    if dfs(x, y, curr_idx+1):
-                        return True
-                    visited.remove((x, y))
             
+            for dr, dc in directions:
+                dx = i + dr
+                dy = j + dc
+                
+                if 0<=dx<row and 0<=dy<col and board[dx][dy] == word[curr_idx] and (dx, dy) not in visited:
+                    visited.add((dx, dy))
+                    if dfs(dx, dy, curr_idx+1):
+                        return True
+                    visited.remove((dx, dy))
             return False
 
+            
         row = len(board)
         col = len(board[0])
+        
         visited = set()
+        
         for i in range(row):
             for j in range(col):
-                if board[i][j] == word[0]:
+                if board[i][j] == word[0] and (i, j) not in visited:
                     visited.add((i, j))
                     if dfs(i, j, 1):
                         return True
                     visited.remove((i, j))
         return False
+        
+        
