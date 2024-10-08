6d4f7ab5f7ef34a377ff4933bd58448b2cd99d63
Garima Arora <garimaarora.ga2@gmail.com>
2024-10-08 23:32:21 +0530

    Time: 838 ms (30.59%), Space: 20 MB (23.85%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -1,19 +1,32 @@
 class Solution:
-    def findAllRecipes(self, recipes, ingredients, supplies):
-        inDegrees = {nodo: 0 for nodo in recipes + list(chain(*ingredients))}
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
         graph = defaultdict(set)
+        numRecipes = len(recipes)
+        in_degree = [0] * numRecipes
+        queue = deque()
+        res = []
+        recipe_index = {recipe: i for i, recipe in enumerate(recipes)}  # Map recipe names to indices
+
+        # Build graph and in-degree list
         for i, recipe in enumerate(recipes):
             for ingredient in ingredients[i]:
                 graph[ingredient].add(recipe)
-                inDegrees[recipe] += 1
-        queue = deque([node for node in inDegrees if inDegrees[node] == 0 and node in supplies])
-        ans = []
+                in_degree[recipe_index[recipe]] += 1
+
+        # Initialize queue with supplies
+        for supply in supplies:
+            if supply in graph:
+                queue.append(supply)
+
+        # Process nodes in topological order
         while queue:
-            curNode = queue.popleft()
-            if curNode in recipes:
-                ans.append(curNode)
-            for node in graph[curNode]:
-                inDegrees[node] -= 1
-                if inDegrees[node] == 0:
-                    queue.append(node)
-        return ans
+            cur_ingredient = queue.popleft()
+
+            for recipe in graph[cur_ingredient]:
+                idx = recipe_index[recipe]
+                in_degree[idx] -= 1
+                if in_degree[idx] == 0:
+                    res.append(recipe)
+                    queue.append(recipe)
+
+        return res
