c539a6bdf4dfd29c6e3bceb09a2bdb1bdff5b8d3
Garima Arora <garimaarora.ga2@gmail.com>
2024-10-08 20:09:02 +0530

    Time: 32 ms (87.35%), Space: 16.5 MB (91.45%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,32 +1,23 @@
 class Solution:
-    """
-    Time complexity: O(N)
-
-    We visit each character in the input at most once and for each character we spend     a constant amount of time.
-
-    Space complexity: O(1)
-
-    We have used only constant space to store the sign and the result.
-    """
     def myAtoi(self, s: str) -> int:
         i = 0
         n = len(s)
-        result = 0
         sign = 1
-        
-        while i < n and s[i] == ' ':
+        ans = 0
+
+        while i < n and s[i] == " ":
             i += 1
-        
-        if i < n and (s[i] == '+' or s[i] == '-'):
+
+        if i < n and (s[i] == '-' or s[i] == '+'):
             sign = -1 if s[i] == '-' else 1
             i += 1
-        
+
         while i < n and s[i].isdigit():
-            digit = int(s[i])
-            
-            if result > (2 ** 31 - 1 - digit) // 10:
+            num = int(s[i])
+            if ans > (2 ** 31 - 1 - num) // 10:
                 return 2 ** 31 - 1 if sign == 1 else -2 ** 31
-            
-            result = result * 10 + digit
+
+            ans = ans * 10 + num
             i += 1
-        return sign * result
+
+        return ans * sign
