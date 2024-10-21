dd747338ad1369a7e11a62d8cbba7cc4f1b20113
Garima Arora <garimaarora.ga2@gmail.com>
2024-10-21 20:37:47 +0530

    Time: 1 ms (99.26%), Space: 17.7 MB (9.28%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -3,11 +3,10 @@ class Solution:
         parent = [i for i in range(1001)]
         
         def find(x):
-            if parent[x] != x:
-                parent[x] = find(parent[x])
-                return parent[x]
-            else:
+            if parent[x] == x:
                 return x
+            parent[x] = find(parent[x])
+            return parent[x]
 
         def union(x, y):
             x_parent, y_parent = find(x), find(y)
@@ -18,7 +17,7 @@ class Solution:
             else:
                 parent[x_parent] = y_parent
             return True
-    
+
         for x,y in edges:
             if not union(x, y):
                 return [x,y]
