72b9ec29397cf0f4835522e04ef762fab5f6bd77
Garima Arora <garimaarora.ga2@gmail.com>
2024-10-22 21:42:02 +0530

    Time: 29 ms (97.06%), Space: 16.9 MB (18.22%) - LeetHub

---
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -0,0 +1,35 @@
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        board.reverse()
+        visited = set()
+        q = deque()
+        q.append((1, 0))
+        visited.add(1)
+        n = len(board)
+        
+        def position(sq):
+            r = (sq - 1) // n
+            c = (sq - 1) % n
+            if r % 2:
+                c = (n - 1 - c)
+            return (r, c)
+
+        while q:
+            curMove, moves = q.popleft()
+            if curMove == n * n:
+                return moves
+            
+            for i in range(1, 7):
+                nextMove = curMove + i
+                if nextMove > n * n:
+                    continue
+                
+                r, c = position(nextMove)
+                if r < n and c < n and board[r][c] != -1:
+                    nextMove = board[r][c]
+                
+                if nextMove not in visited:
+                    visited.add(nextMove)
+                    q.append((nextMove, moves + 1))
+        
+        return -1
