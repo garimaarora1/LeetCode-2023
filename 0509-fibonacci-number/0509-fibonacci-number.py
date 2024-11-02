99369217170a8d7cb578d41a2b1079b0b38f1171
Garima Arora <garimaarora.ga2@gmail.com>
2024-11-02 11:58:05 +0530

    Time: 45 ms (32.28%), Space: 16.7 MB (5.42%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -4,8 +4,7 @@ class Solution:
         
         if n == 0 or n == 1:
                 return n
-        dp[1] = 1
-        
+        dp[1] = 1     
         for i in range(2, n+1):
             dp[i] = dp[i-1] + dp[i-2]
         
