8bcce5bfab32673f99d008420c9c07343bb7a60f
Garima Arora <garimaarora.ga2@gmail.com>
2024-11-06 15:37:40 +0530

    Time: 519 ms (10.68%), Space: 119.4 MB (5.33%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -9,14 +9,15 @@ class Solution:
             if r >= row or c >= col:
                 return 0
             
-            if (r, c) not in cache:
-                down = helper(r+1, c)
-                right = helper(r, c+1)
-                diagonal = helper(r+1, c+1)
+            if (r, c) in cache:
+                return cache[(r, c)]
+            down = helper(r+1, c)
+            right = helper(r, c+1)
+            diagonal = helper(r+1, c+1)
 
-                cache[(r, c)] = 0
-                if matrix[r][c] == '1':
-                    cache[(r, c)] = 1 + min(right, down, diagonal)
+            cache[(r, c)] = 0
+            if matrix[r][c] == '1':
+                cache[(r, c)] = 1 + min(right, down, diagonal)
             
             return cache[(r, c)]
         
