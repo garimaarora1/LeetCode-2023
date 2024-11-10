203ed5421164f84f6fe4531909d42ca62660f467
Garima Arora <garimaarora.ga2@gmail.com>
2024-11-10 22:38:30 +0530

    Time: 88 ms (94.93%), Space: 45.7 MB (39.09%) - LeetHub

--- 1845-seat-reservation-manager/1845-seat-reservation-manager.py
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.py
@@ -1,19 +1,23 @@
 class SeatManager:
+    def __init__(self, n):
+        # Set marker to the first unreserved seat.
+        self.marker = 1
 
-    def __init__(self, n: int):
-        self.seats = [i for i in range(1, n+1)]
+        # Min heap to store all unreserved seats.
+        self.available_seats = []
 
-    def reserve(self) -> int:
-        
-        seat_number = heapq.heappop(self.seats)
+    def reserve(self):
+        # If min-heap has any element in it, then,
+        # get the smallest-numbered unreserved seat from the min heap.
+        if self.available_seats:
+            seat_number = heapq.heappop(self.available_seats)
+            return seat_number
+
+        # Otherwise, the marker points to the smallest-numbered seat.
+        seat_number = self.marker
+        self.marker += 1
         return seat_number
 
-    def unreserve(self, seatNumber: int) -> None:
-        heapq.heappush(self.seats, seatNumber)
-        
-
-
-# Your SeatManager object will be instantiated and called as such:
-# obj = SeatManager(n)
-# param_1 = obj.reserve()
-# obj.unreserve(seatNumber)
+    def unreserve(self, seat_number):
+        # Push unreserved seat in the min heap.
+        heapq.heappush(self.available_seats, seat_number)
