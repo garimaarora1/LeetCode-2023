495583d4e73de1a78dbe62b265fa14d83b81bc8d
Garima Arora <garimaarora.ga2@gmail.com>
2024-11-11 11:01:20 +0530

    Time: 675 ms (27.86%), Space: 29.1 MB (83.31%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -8,4 +8,5 @@ class Solution:
             max_price_1 = max(max_price_1, price-buy_price_1)
             buy_price_2 = min(buy_price_2, price-max_price_1)
             max_price_2 = max(max_price_2, price-buy_price_2)
+            print(buy_price_1, max_price_1, buy_price_2, max_price_2)
         return max_price_2
